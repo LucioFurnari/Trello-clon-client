eb7afe69d56d7d5a9b260295c5b0fbf7f817483d
LucioFurnari <luciofurnari99@gmail.com>
2024-07-07 15:38:11 -0300

    feat: add action to set status of container

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -1,10 +1,11 @@
-interface NavbarButtonProps {
+export interface NavbarButtonProps {
   name: string,
+  action: (value: any) => void
 }
 
-export default function NavbarButton({name}: NavbarButtonProps) {
+export default function NavbarButton({name, action}: NavbarButtonProps) {
   return (
-    <button className="p-2">
+    <button className="p-2" onClick={action}>
       {name}
     </button>
   )
