4f24a1a3c86321acec1a52c8342bbe13d20d5c9f
LucioFurnari <luciofurnari99@gmail.com>
2024-07-07 15:10:37 -0300

    feat: create subMenu component

---
+++ src/components/Navbar/SubMenu.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+interface SubMenuItem {
+  title: string
+}
+
+interface SubMenu {
+  title?: string,
+  list?: SubMenuItem[],
+  isMenuOpen: boolean
+}
+
+export default function SubMenu({ title, list, isMenuOpen }: SubMenu) {
+
+  return (
+    <div className={isMenuOpen ? 'block' : 'hidden'}>
+      <div className="bg-gray-600">
+      <h2>{title}</h2>
+      <div>
+        {
+          list?.map((item, index) => {
+            return(
+              <div key={index}>
+                <h3>{item.title}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+    </div>
+  )
+}
