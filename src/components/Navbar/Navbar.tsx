62a4c91a8964cef5241fa3c4b2fd9475f9cbab09
LucioFurnari <luciofurnari99@gmail.com>
2024-07-08 19:20:41 -0300

    feat: import and implement NavBarLink component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import SubMenuContainer from "./SubMenuContainer"
+import NavBarLink from "./NavBarLink"
 
 export default function Navbar() {
   return (
     <nav className="flex bg-slate-700 p-2">
-      <SubMenuContainer name={'Workspace'} />
+      <SubMenuContainer title="Your Workspaces" name={'Workspace'} />
+      <NavBarLink link={"/login"} name={"Login"} />
+      <NavBarLink link={"/register"} name={"Register"} />
     </nav>
   )
 }
