7c6ff6912ad1bed801750114e43843b8fcd4627e
LucioFurnari <luciofurnari99@gmail.com>
2024-07-08 15:23:28 -0300

    feat: add style class

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import SubMenuContainer from "./SubMenuContainer"
 
 export default function Navbar() {
   return (
-    <nav className="flex bg-slate-700">
+    <nav className="flex bg-slate-700 p-2">
       <SubMenuContainer name={'Workspace'} />
     </nav>
   )
--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -5,7 +5,7 @@ export interface NavbarButtonProps {
 
 export default function NavbarButton({name, action}: NavbarButtonProps) {
   return (
-    <button className="p-2" onClick={action}>
+    <button className="p-2 px-4 text-slate-400 hover:bg-slate-600 transition-colors rounded-md" onClick={action}>
       {name}
     </button>
   )
