256f247aa3ff5140af8c1172ebebd17aefecf3fe
LucioFurnari <luciofurnari99@gmail.com>
2024-07-08 19:39:58 -0300

    feat: add svg icon

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -1,3 +1,5 @@
+import { SVGProps } from "react"
+
 export interface NavbarButtonProps {
   name: string,
   action: (value: any) => void
@@ -6,7 +8,14 @@ export interface NavbarButtonProps {
 export default function NavbarButton({name, action}: NavbarButtonProps) {
   return (
     <button className="p-2 px-4 text-slate-400 hover:bg-slate-600 transition-colors rounded-md" onClick={action}>
-      {name}
+      <span>{name}</span>
+      <span><IcOutlineKeyboardArrowDown /></span>
     </button>
+  )
+}
+
+function IcOutlineKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className="inline" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z"></path></svg>
   )
 }
