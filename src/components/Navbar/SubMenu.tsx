2eaec06ff99b01d4f891ff28829c96a9408d90af
LucioFurnari <luciofurnari99@gmail.com>
2024-07-09 19:26:54 -0300

    feat: remove condition in className, add conditional rendering

--- src/components/Navbar/SubMenu.tsx
+++ src/components/Navbar/SubMenu.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from "next/link"
+
 interface SubMenuItem {
   title: string
 }
@@ -13,21 +15,24 @@ interface SubMenu {
 export default function SubMenu({ title, list, isMenuOpen }: SubMenu) {
 
   return (
-    <div className="relative">
-      <div className={isMenuOpen ? 'absolute top-2 left-1 bg-slate-600' : 'hidden'}>
-      <h2>{title}</h2>
-      <div>
-        {
-          list?.map((item, index) => {
-            return(
-              <div key={index}>
-                <h3>{item.title}</h3>
-              </div>
-            )
-          })
-        }
+    <>
+      {
+      isMenuOpen &&
+      <div className='absolute top-[52px] left-1 bg-slate-600 min-w-72 px-2'>
+        <h2 className="text-sm text-slate-400 m-3 ml-6">{title}</h2>
+        <ul>
+          {
+            list?.map((item, index) => {
+              return(
+                <li className="m-2" key={index}>
+                  <Link className=" text-slate-300 hover:bg-slate-500 p-4 w-full inline-block rounded" href={`/workspace/${item.title}`} >{item.title}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
-    </div>
-    </div>
+      }
+    </>
   )
 }
