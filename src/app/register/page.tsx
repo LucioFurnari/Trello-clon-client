9c05df95829dbf751f057f49e3382673fc23fb76
LucioFurnari <luciofurnari99@gmail.com>
2024-07-11 15:47:28 -0300

    feat: import RegisterForm component

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,10 @@
-
+import RegisterForm from "@/components/Register/RegisterForm"
 
 export default function Register() {
   return (
     <main>
       <h1>Register page</h1>
+      <RegisterForm />
     </main>
   )
 }
