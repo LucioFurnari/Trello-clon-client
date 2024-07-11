5302021db1cc9534de8e92df10f55f04c06047cf
LucioFurnari <luciofurnari99@gmail.com>
2024-07-10 21:52:41 -0300

    feat: import LoginForm component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
-
+import { loginUser } from "@/lib/auth"
+import LoginForm from "@/components/Login/LoginForm"
 
 export default function Login() {
   return (
     <main>
       <h1>Login page</h1>
+      <LoginForm formAction={loginUser} />
     </main>
   )
 }
