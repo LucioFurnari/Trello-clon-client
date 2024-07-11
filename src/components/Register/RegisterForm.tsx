2a1afb1295ec36d9c06b5483f2284e3e4143543f
LucioFurnari <luciofurnari99@gmail.com>
2024-07-11 16:41:11 -0300

    feat: add useFormState hook, change component to client

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -1,14 +1,28 @@
+'use client'
+
 import { registerUser } from "@/lib/auth"
 import { SubmitButton } from "./SubmitButton"
+import { useFormState } from "react-dom"
+
+const initialState = {
+  success: false,
+  message: ''
+}
 
 export default function RegisterForm() {
+  const [state, formAction] = useFormState(registerUser, initialState)
+
 
   return (
-    <form action={registerUser}>
+    <form action={formAction}>
       <input type="text"  name="username"  id="username" placeholder="username" required/>
       <input type="email" name="email"     id="email"    placeholder="email"    required/>
       <input type="text"  name="password"  id="password" placeholder="password" required/>
       <SubmitButton />
+      {
+        state?.success &&
+        <p>{state.message}</p>
+      }
     </form>
   )
 }
