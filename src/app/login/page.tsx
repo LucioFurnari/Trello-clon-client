dcada9ed674951fd220f5c751653e8dafe4e090e
LucioFurnari <luciofurnari99@gmail.com>
2024-07-10 22:17:39 -0300

    feat: remove prop from component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,10 @@
-import { loginUser } from "@/lib/auth"
 import LoginForm from "@/components/Login/LoginForm"
 
 export default function Login() {
   return (
     <main>
       <h1>Login page</h1>
-      <LoginForm formAction={loginUser} />
+      <LoginForm />
     </main>
   )
 }
