0c2be6d6d56869c98e0cc1d7c1ca8c913b03b9d0
LucioFurnari <luciofurnari99@gmail.com>
2024-07-13 19:11:48 -0300

    feat: change component to client, import useUserContext

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,26 @@
+'use client'
+
 import SubMenuContainer from "./SubMenuContainer"
-import NavBarLink from "./NavBarLink"
+import NavLinkContainer from "./NavBarLinkContainer/NavLinkContainer"
+import HomeLink from "./HomeLink"
+import UserMenuContainer from "./UserMenu/LoginMenuContainer"
+import { useUserContext } from "@/context/UserContext"
 
 export default function Navbar() {
+  const { user } = useUserContext();
+
   return (
-    <nav className="flex justify-between bg-slate-700 p-2">
-      <SubMenuContainer title="Your Workspaces" name={'Workspace'} />
-      <div>
-        <NavBarLink link={"/"} name={"Home"} />
-        <NavBarLink link={"/login"} name={"Login"} />
-        <NavBarLink link={"/register"} name={"Register"} />
-      </div>
+    <nav className="flex items-center justify-between bg-slate-700 p-2">
+      <HomeLink />
+      {
+        user.loggedIn ?
+        <>
+          <SubMenuContainer title="Your Workspaces" name={'Workspace'} />
+          <UserMenuContainer />
+        </>
+        :
+          <NavLinkContainer />
+      }
     </nav>
   )
 }
