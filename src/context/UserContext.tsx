ce692297a6f0a404b3fff7a40dc81374143871aa
LucioFurnari <luciofurnari99@gmail.com>
2024-07-13 18:25:05 -0300

    feat: import verifyToken function, add useEffect hook

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,9 +1,13 @@
-import { createContext, useState, useContext } from "react";
+'use client'
+
+import { createContext, useState, useContext, useEffect } from "react";
+import { verifyToken } from "@/lib/auth";
 
 interface UserData {
   username: string,
   email: string,
-  id: number
+  id: number,
+  loggedIn: boolean
 }
 
 type UserContextType = {
@@ -18,7 +22,17 @@ type UserContextProviderProps = {
 const UserContext = createContext<UserContextType | null>(null);
 
 export default function UserContextProvider({ children }: UserContextProviderProps) {
-  const [user, setUser] = useState<UserData>({ username: '', email: '', id: 0 });
+  const [user, setUser] = useState<UserData>({ username: '', email: '', id: 0, loggedIn: false });
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const userData = await verifyToken()
+
+      console.log(userData)
+      if (userData) setUser({ username: userData.username, email: userData.email, id: userData.id, loggedIn: true })
+    }
+    getUserData();
+  }, [])
 
   return (
     <UserContext.Provider
