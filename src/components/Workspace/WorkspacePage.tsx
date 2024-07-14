5e1adf6831d5e7f5d3eb9df4d55175dc8adf42d9
LucioFurnari <luciofurnari99@gmail.com>
2024-07-14 20:59:48 -0300

    feat: import getWorkspace function, change component to be async

--- src/components/Workspace/WorkspacePage.tsx
+++ src/components/Workspace/WorkspacePage.tsx
@@ -1,9 +1,16 @@
+import { getWorkspace } from "@/lib/workspace"
+import WorkspaceHeader from "./WorkspaceHeader"
 
+interface WorkspacePageProp {
+  workspaceId: string
+}
 
-export default function WorkspacePage() {
+export default async function WorkspacePage({ workspaceId }: WorkspacePageProp) {
+  const workspace = await getWorkspace(workspaceId);
+  
   return (
     <main>
-      
+      <WorkspaceHeader title={workspace.name} description={workspace.description} visibilityPrivate={workspace.visibilityPrivate} visibilityPublic={workspace.visibilityPublic} />
     </main>
   )
 }
