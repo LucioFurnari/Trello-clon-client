af8061652ad6bbc3efb9f407476205d0422efe09
LucioFurnari <luciofurnari99@gmail.com>
2024-07-14 21:00:10 -0300

    feat: add visibility values

--- src/components/Workspace/WorkspaceHeader.tsx
+++ src/components/Workspace/WorkspaceHeader.tsx
@@ -1,13 +1,23 @@
 interface WorkspaceHeaderProps {
   title: string,
-  description: string
+  description: string,
+  visibilityPrivate: boolean,
+  visibilityPublic: boolean
 }
 
-export default function WorkspaceHeader({ title, description }: WorkspaceHeaderProps) {
+export default function WorkspaceHeader({ title, description, visibilityPrivate, visibilityPublic }: WorkspaceHeaderProps) {
   return (
     <header>
       <h1>{title}</h1>
       <p>{description}</p>
+      <span>
+        {
+          visibilityPrivate && 'Private'
+        }
+        {
+          visibilityPublic && 'Public'
+        }
+      </span>
     </header>
   )
 }
