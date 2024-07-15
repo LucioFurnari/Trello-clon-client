e0c9a58901ecb01b95e53bba2ade13e30bcbb4d7
LucioFurnari <luciofurnari99@gmail.com>
2024-07-15 16:47:51 -0300

    feat: import redirect method, change loginUser function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,51 @@
 'use server'
 
 import { getCookie, setCookie } from "./cookies";
+import { redirect } from "next/navigation";
 
 export async function loginUser(formData: FormData) {
   'use server'
 
-  const rawFormData = {
-    email: formData.get('email'),
-    password: formData.get('password')
+  try {
+    const email = formData.get('email') as string | null;
+    const password = formData.get('password') as string | null;
+    
+    if (!email || !password) {
+      throw new Error('Email and password must be provided');
+    }
+  
+    const rawFormData = { email, password };
+
+    const res = await fetch(`${process.env.API_HOST}/login`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(rawFormData)
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+      if (data.errorList) {
+        console.error('Validation Errors:', data.errorList);
+        throw new Error(data.errorList);
+      } else {
+        throw new Error(data.message || 'Login failed');
+      }
+    }
+  
+    if (data.token) {
+      setCookie(data.token);
+      redirect('/')
+    } else {
+      throw new Error('Token not received');
+    }
+
+  } catch (error) {
+    console.error('Error during login');
   }
-
-  const res = await fetch(`${process.env.API_HOST}/login`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(rawFormData)
-  })
-
-  const data = await res.json()
-
-  if (data.token) {
-    setCookie(data.token);
-  }
-
-  console.log(data)
 }
 
 export async function registerUser(prevState: any, formData: FormData) {
