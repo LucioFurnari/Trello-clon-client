d4b35289972d5f043790af75cdca519d8923122e
LucioFurnari <luciofurnari99@gmail.com>
2024-07-17 11:14:18 -0300

    feat: change component tu client, add conditional rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
 import HomePage from "@/components/HomePage";
+import UserHomePage from "@/components/UserHomePage";
+import { useUserContext } from "@/context/UserContext";
 
 export default function Home() {
+  const { user } = useUserContext();
+
   return (
-    <HomePage />
+    <>
+    {
+      user.loggedIn ?
+      <UserHomePage />
+      :
+      <HomePage />
+    }
+    </>
   );
 }
