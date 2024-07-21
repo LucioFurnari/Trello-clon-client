2ddfa80ee245ec64b01e8483d59435f39e77ff0f
LucioFurnari <luciofurnari99@gmail.com>
2024-07-21 15:00:27 -0300

    fix: add try catch for error handling

--- src/lib/workspace.ts
+++ src/lib/workspace.ts
@@ -1,35 +1,45 @@
 import { getCookie } from "./cookies"
 
 export async function getWorkspace(workspaceId: string) {
-  const token = await getCookie();
+  try {
+    const token = await getCookie();
 
-  const res = await fetch(`${process.env.API_HOST}/workspace/${workspaceId}`,{
-    method: 'GET',
-    headers: {
-      'Content-Type': 'Content-Type',
-      Cookie: `access_token=${token}`
-    }
-  });
+    const res = await fetch(`${process.env.API_HOST}/workspace/${workspaceId}`,{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'Content-Type',
+        Cookie: `access_token=${token}`
+      }
+    });
 
-  if (!res.ok) return null;
+    if (!res.ok) return null;
 
-  const data = await res.json();
-  return data.workspace;
+    const data = await res.json();
+    return data.workspace;
+  } catch (error) {
+    console.error('Error in the server, it was not possible to perform the fetch');
+    return null;
+  }
 }
 
 export async function getAllWorkspacesOfUser() {
-  const token = await getCookie();
+  try {
+    const token = await getCookie();
 
-  const res = await fetch(`${process.env.API_HOST}/workspace`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'Content-Type',
-      Cookie: `access_token=${token}`
-    }
-  });
+    const res = await fetch(`${process.env.API_HOST}/workspace`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'Content-Type',
+        Cookie: `access_token=${token}`
+      }
+    });
 
-  if (!res.ok) return null;
+    if (!res.ok) return null;
 
-  const data = await res.json();
-  return data.workspaces
+    const data = await res.json();
+    return data.workspaces
+  } catch (error) {
+    console.error('Error in the server, it was not possible to perform the fetch')
+    return null;
+  }
 }
