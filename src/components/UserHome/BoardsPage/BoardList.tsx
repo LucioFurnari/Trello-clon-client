27109e6bb72e991474a745c3c618025dfd9561cd
LucioFurnari <luciofurnari99@gmail.com>
2024-07-20 22:47:52 -0300

    feat: add conditional rendering

--- src/components/UserHome/BoardsPage/BoardList.tsx
+++ src/components/UserHome/BoardsPage/BoardList.tsx
@@ -4,6 +4,7 @@ export default function BoardList({ boards }: { boards?: any }) {
   return (
     <ul>
       {
+        boards &&
         boards.map((board: any) => <BoardItem title={board.title} key={board.boardId}/>)
       }
     </ul>
