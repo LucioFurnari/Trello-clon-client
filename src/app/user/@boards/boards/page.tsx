7b83577228fecb6deb0d3ffa6445492ecb255ac3
LucioFurnari <luciofurnari99@gmail.com>
2024-07-21 15:23:00 -0300

    feat: change component to use server side props

--- src/app/user/@boards/boards/page.tsx
+++ src/app/user/@boards/boards/page.tsx
@@ -1,9 +1,25 @@
+import { getAllWorkspacesOfUser } from "@/lib/workspace"
+import WorkspacesList from "@/components/UserHome/BoardsPage/WorkspacesList";
 
+export async function getServerSideProps() {
+  const data = await getAllWorkspacesOfUser();
 
-export default function BoardsPage() {
+  return {
+    props: {
+      data
+    }
+  };
+}
+
+export default async function BoardsPage({ data }: {data:any}) {
+
+  console.log(data)
+
   return (
-    <>
+    <div className="flex flex-col p-4">
       <h1>This is the list of the user boards</h1>
-    </>
+      <h2>Your Workspaces</h2>
+      <WorkspacesList workspaces={data} />
+    </div>
   )
 }
