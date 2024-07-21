7f72923f2f8afc247dd1bb9feeb0749c4af52fa0
LucioFurnari <luciofurnari99@gmail.com>
2024-07-20 22:47:29 -0300

    feat: change component and fix key value

--- src/components/UserHome/BoardsPage/WorkspacesList.tsx
+++ src/components/UserHome/BoardsPage/WorkspacesList.tsx
@@ -1,9 +1,21 @@
 import { WorkspaceItem } from "./WorkspaceItem"
 
-export default async function WorkspacesList() {
+export default async function WorkspacesList({ workspaces }: { workspaces: any}) {
   return (
     <div>
-
+      {
+        workspaces &&
+        workspaces.map((item: any) => {
+          return (
+            <WorkspaceItem 
+              name={item.workspace.name} 
+              key={item.workspace.workspaceId} 
+              boards={item.workspace.boards}
+            />
+          )
+        }
+        )
+      }
     </div>
   )
 }
