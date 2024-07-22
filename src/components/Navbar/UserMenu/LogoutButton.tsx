46d356e7cfd918f65ab64045321ea201a5099c69
LucioFurnari <luciofurnari99@gmail.com>
2024-07-22 13:02:31 -0300

    feat: add setIsLogged function

--- src/components/Navbar/UserMenu/LogoutButton.tsx
+++ src/components/Navbar/UserMenu/LogoutButton.tsx
@@ -1,9 +1,12 @@
 import { logout } from "@/lib/auth";
+import { useUserContext } from "@/context/UserContext";
 
 export default function LogoutButton() {
+  const { setIsLogged } = useUserContext();
 
   async function handleLogout() {
     await logout()
+    setIsLogged(false)
   }
 
   return (
