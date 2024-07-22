b55130a5d953d0e73504234da5bc0b850f7fb23b
LucioFurnari <luciofurnari99@gmail.com>
2024-07-22 16:00:57 -0300

    feat: change the component to use Link, add style

--- src/components/UserHome/BoardsPage/BoardItem.tsx
+++ src/components/UserHome/BoardsPage/BoardItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 interface BoardItemProp {
   title: string,
@@ -5,8 +6,15 @@ interface BoardItemProp {
 
 export default function BoardItem({ title }: BoardItemProp) {
   return (
-    <li>
-      <p>{title}</p>
+    <li className="bg-pink-500 cursor-pointer">
+      <Link 
+        href={""} 
+        className="inline-block pb-10 pr-10 pt-2 pl-2">
+        { title }
+      </Link>
     </li>
   )
 }
+
+// pb-10 pr-10 pt-2 pl-2
+//  h-20 pt-2 pl-2 pr-10
