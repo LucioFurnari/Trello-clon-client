1edf87cefbcdaf25979231d0adb0b3d17b5b39f6
LucioFurnari <luciofurnari99@gmail.com>
2024-07-24 19:47:15 -0300

    feat: change component to client side, add react hooks

--- src/components/Board/ListContainer.tsx
+++ src/components/Board/ListContainer.tsx
@@ -1,16 +1,27 @@
+'use client'
+
 import List from "./List"
+import AddListButton from "./Buttons/AddListButton"
+import { useEffect, useState } from "react"
 
 interface ListContainerProps {
   lists: [],
 }
 
 export default function ListContainer({ lists }: ListContainerProps) {
+  const [listArray, setListArray] = useState([])
+
+  useEffect(() => {
+    setListArray(lists)
+  }, [lists]);
+
   return (
     <section className="flex flex-row">
       {
-        lists &&
-        lists.map((item: any) => <List key={item.listId} name={item.name} cards={item.cards} />)
+        listArray &&
+        listArray.map((item: any) => <List key={item.listId} name={item.name} cards={item.cards} />)
       }
+      <AddListButton setAction={setListArray} />
     </section>
   )
 }
