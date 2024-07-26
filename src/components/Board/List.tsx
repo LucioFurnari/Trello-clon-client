3a6803fec7b96fff109849975f18fa8608ca37bd
LucioFurnari <luciofurnari99@gmail.com>
2024-07-26 16:31:12 -0300

    feat: add delete list and addCard buttons, add id propertie to interface

--- src/components/Board/List.tsx
+++ src/components/Board/List.tsx
@@ -1,18 +1,23 @@
 import Card from "./Card"
+import AddCardButton from "./Buttons/AddCardButton"
+import DeleteListButton from "./Buttons/DeleteListButton"
 
 interface ListProps {
   name: string,
+  id: string,
   cards: []
 }
 
-export default function List({ name, cards }: ListProps) {
+export default function List({ name, cards, id }: ListProps) {
   return (
-    <div className="mx-6 p-4 bg-slate-700 w-52">
+    <div className="mx-6 p-4 bg-slate-700 w-52" draggable={true}>
       <h2>{name}</h2>
+      <DeleteListButton listId={id} />
       {
         cards && 
         cards.map((item: any) => <Card key={item.cardId} title={item.title} /> )
       }
+      <AddCardButton />
     </div>
   )
 }
