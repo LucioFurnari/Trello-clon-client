80956a966ca9bc0b8e833ea5b6b028206657469a
LucioFurnari <luciofurnari99@gmail.com>
2024-07-28 23:49:31 -0300

    feat: create update position function

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -24,9 +24,9 @@ export async function addList(boardId: string, name: string) {
   }
 }
 
-export async function deleteList(listId: string) {
+export async function deleteList(listId: string, boardId: string) {
   try {
-    const res = await fetch(`${process.env.API_HOST}/list/${listId}`, {
+    const res = await fetch(`${process.env.API_HOST}/board/${boardId}/list/${listId}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -40,6 +40,28 @@ export async function deleteList(listId: string) {
 
     const data = await res.json();
     return data.list;
+  } catch (error) {
+    console.error('Error in the server, it was not possible to perform the fetch');
+    return null;
+  }
+}
+
+export async function updatePosition(list: any) {
+  try {
+    const newList =  JSON.stringify({ newList: list} )
+    const res = await fetch(`${process.env.API_HOST}/list`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: newList
+    })
+
+    if (!res.ok) {
+      console.error('Failed to update list', res.status, res.statusText)
+      return null;
+    }
+    console.log('working')
   } catch (error) {
     console.error('Error in the server, it was not possible to perform the fetch');
     return null;
