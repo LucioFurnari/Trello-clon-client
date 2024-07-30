e6d91f9175dbd920c78843fd9cc9a6cc01418f40
LucioFurnari <luciofurnari99@gmail.com>
2024-07-30 18:56:12 -0300

    feat: add createCard function to call api

--- src/lib/card.ts
+++ src/lib/card.ts
@@ -1,6 +1,25 @@
+"use server"
 
+export async function createCard(listId: string, card: any) {
+  try {
+    const newCard = JSON.stringify(card);
+    const res = await fetch(`${process.env.API_HOST}/list/${listId}/card`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: newCard
+    })
 
+    if (res.status !== 201) {
+      console.error('Failed to create card', res.status, res.statusText)
+      return null;
+    }
 
-export async function createCard() {
-  
+    const data = await res.json();
+    return data.card;
+  } catch (error) {
+    console.error('Error in the server, it was not possible to perform the fetch');
+    return null;
+  }
 }
