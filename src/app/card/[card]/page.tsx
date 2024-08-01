02f157be26be10872aa3065e64e82994b87f973f
LucioFurnari <luciofurnari99@gmail.com>
2024-08-01 15:48:47 -0300

    feat: create dynamic card route

---
+++ src/app/card/[card]/page.tsx
@@ -0,0 +1,11 @@
+import Modal from "@/components/Modal/Modal"
+
+export default function CardPage() {
+  return (
+    <section>
+      <Modal>
+        <h1>This is the card page !</h1>
+      </Modal>
+    </section>
+  )
+}
