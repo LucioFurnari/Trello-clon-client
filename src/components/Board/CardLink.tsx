f66cbcc355ba64e6f3f65236dcb75c707d9ce0f6
LucioFurnari <luciofurnari99@gmail.com>
2024-08-01 23:01:14 -0300

    fix: fix missing cardId

--- src/components/Board/CardLink.tsx
+++ src/components/Board/CardLink.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link"
 
 interface CardProps {
-  title: string
+  title: string,
+  cardId: string
 }
 
-export default function CardLink({title}: CardProps) {
+export default function CardLink({title, cardId}: CardProps) {
   return (
-    <Link className=" inline-block w-full border-2 border-black hover:bg-slate-400 p-2 rounded-md mb-2" href={`/card/${title}`}>{title}</Link>
+    <Link className=" inline-block w-full border-2 border-black hover:bg-slate-400 p-2 rounded-md mb-2" href={`/card/${cardId}`}>{title}</Link>
   )
 }
