3cdc2424720a71e1c8d25431e60a82891756fa0c
LucioFurnari <luciofurnari99@gmail.com>
2024-08-02 23:44:44 -0300

    feat: create Text editor component for card description

---
+++ src/components/Editor/TextEditor.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const MyEditor: React.FC = () => {
+  const [content, setContent] = useState<string>('');
+
+  return (
+    <div>
+      <ReactQuill value={content} onChange={setContent} />
+      <button onClick={() => console.log(content)}>Log Content</button>
+    </div>
+  );
+};
+
+export default MyEditor;
