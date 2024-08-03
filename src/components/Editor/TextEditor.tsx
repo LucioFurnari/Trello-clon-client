c73fdfeb9cbb026a44cb1c5f6ec3b57aff2b9ef0
LucioFurnari <luciofurnari99@gmail.com>
2024-08-02 23:49:19 -0300

    feat: add more elements to the editor

--- src/components/Editor/TextEditor.tsx
+++ src/components/Editor/TextEditor.tsx
@@ -2,12 +2,36 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+  toolbar: [
+    [{ header: '1'}, { header: '2'}, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered'}, { list: 'bullet' }],
+    ['link'],
+    ['clean']
+  ],
+};
+
+const formats = [
+  'header', 'font', 'size',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet',
+  'link',
+];
+
+
 const MyEditor: React.FC = () => {
   const [content, setContent] = useState<string>('');
 
   return (
     <div>
-      <ReactQuill value={content} onChange={setContent} />
+      <ReactQuill 
+        value={content} 
+        onChange={setContent} 
+        modules={modules}
+        formats={formats}
+      />
       <button onClick={() => console.log(content)}>Log Content</button>
     </div>
   );
