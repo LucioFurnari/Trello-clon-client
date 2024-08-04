ef6086b59a684ebca00c2aff94a0f559c086f1ff
LucioFurnari <luciofurnari99@gmail.com>
2024-08-04 20:24:08 -0300

    feat: change style

--- src/components/Board/CardLink.tsx
+++ src/components/Board/CardLink.tsx
@@ -7,6 +7,6 @@ interface CardProps {
 
 export default function CardLink({title, cardId}: CardProps) {
   return (
-    <Link className=" inline-block w-full border-2 border-black hover:bg-slate-400 p-2 rounded-md mb-2" href={`/card/${cardId}`}>{title}</Link>
+    <Link className="inline-block w-full bg-gray-500 text-slate-200 hover:bg-slate-400 p-2 rounded-md mb-4" href={`/card/${cardId}`}>{title}</Link>
   )
 }
