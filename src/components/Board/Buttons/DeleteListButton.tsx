209f79a62e34a4467cbf8b23d929b9d66ca02f54
LucioFurnari <luciofurnari99@gmail.com>
2024-08-05 20:29:49 -0300

    feat: import message modal, add functions for state

--- src/components/Board/Buttons/DeleteListButton.tsx
+++ src/components/Board/Buttons/DeleteListButton.tsx
@@ -3,6 +3,8 @@
 import { deleteList } from "@/lib/list"
 import { useListContext } from "@/context/ListContext"
 import { useParams } from "next/navigation"
+import MessageModal from "@/components/Modal/MessageModal"
+import { useState } from "react"
 
 interface DeleteListButtonProps {
   listId: string,
@@ -10,6 +12,7 @@ interface DeleteListButtonProps {
 
 export default function DeleteListButton({ listId }: DeleteListButtonProps) {
   const { list, setList } = useListContext();
+  const [openModal, setOpenModal] = useState(false);
   const params = useParams<{ board: string}>();
 
   async function handleDeleteList() {
@@ -21,9 +24,28 @@ export default function DeleteListButton({ listId }: DeleteListButtonProps) {
 
   }
 
+  function handleOpenModal(){
+    setOpenModal(true)
+  }
+
+  function handleCloseModal() {
+    setOpenModal(false)
+  }
+
   return (
-    <button onClick={handleDeleteList}>
-      Delete card
-    </button>
+    <div>
+      {openModal &&
+        <MessageModal setAction={handleCloseModal}>
+          <h2 className="text-lg font-semibold">Are you sure you want to delete the list?</h2>
+          <p>(All the cards it contains will be deleted.)</p>
+          <button className="bg-red-600 hover:bg-red-500 p-4 rounded-md mt-4 font-semibold" onClick={handleDeleteList}>
+            Delete List
+          </button>
+        </MessageModal>
+      }
+      <button className="text-gray-200 hover:bg-slate-400 p-2 rounded" onClick={handleOpenModal}>
+        X
+      </button>
+    </div>
   )
 }
