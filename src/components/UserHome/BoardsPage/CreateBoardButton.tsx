25b234cce72d0e9e66481ff590d466264587b70e
LucioFurnari <luciofurnari99@gmail.com>
2024-08-06 17:53:28 -0300

    feat: add Modal to create board

--- src/components/UserHome/BoardsPage/CreateBoardButton.tsx
+++ src/components/UserHome/BoardsPage/CreateBoardButton.tsx
@@ -1,9 +1,32 @@
+"use client"
 
+import { useState } from "react"
+import MessageModal from "@/components/Modal/MessageModal"
 
 export default function CreateBoardButton() {
+  const [openModal, setOpenModal] = useState(false);
+
+  function handleOpenModal() {
+    setOpenModal(true)
+  }
+
+  function handleCloseModal() {
+    setOpenModal(false);
+  }
+
   return (
-    <button className="bg-zinc-700 text-zinc-300 text-sm hover:bg-zinc-600 p-10">
-      Create new board
-    </button>
+    <>
+      <button onClick={handleOpenModal} className="bg-zinc-700 text-zinc-300 text-sm hover:bg-zinc-600 p-10">
+        Create new board
+      </button>
+      {
+        openModal &&
+        <MessageModal setAction={handleCloseModal}>
+          <form>
+            <input type="text" placeholder="Board title" />
+          </form>
+        </MessageModal>
+      }
+    </>
   )
 }
