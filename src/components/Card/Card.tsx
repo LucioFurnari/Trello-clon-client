883b12ba63a4fa6a6104f580c64df53d2fd75419
LucioFurnari <luciofurnari99@gmail.com>
2024-08-08 17:33:53 -0300

    feat: import DateSection

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
 import Modal from "../Modal/Modal"
 import DescriptionSection from "./DescriptionSection";
+import DateSection from "./DateSection";
 import { getCard } from "@/lib/card"
 
 export default async function Card({ cardId }: { cardId: string}) {
@@ -7,11 +8,11 @@ export default async function Card({ cardId }: { cardId: string}) {
 
   return (
     <Modal>
-      <article className="p-4">
-        <h1>{card.title}</h1>
-        <p>Card id: {card.cardId}</p>
+      <div className="card p-4 rounded-lg shadow-md bg-white">
+        <h3 className="text-lg font-bold">{card.title}</h3>
+        <DateSection dueDate={card.dueDate} />
         <DescriptionSection description={card.description} />
-      </article>
+      </div>
     </Modal>
   )
 }
