e4166052049c711296467b376d5ea94a1eec29bd
LucioFurnari <luciofurnari99@gmail.com>
2024-08-08 20:40:00 -0300

    feat: Add remains days to due date, and style

--- src/components/Card/DateSection.tsx
+++ src/components/Card/DateSection.tsx
@@ -1,20 +1,35 @@
 
 
-export default function DateSection({ dueDate }: { dueDate: Date | null}) {
+export default function DateSection({ dueDate }: { dueDate: Date | null }) {
   const isOverdue = dueDate ? new Date(dueDate) < new Date() : false;
-  const isDueSoon = dueDate
-    ? new Date(dueDate).getTime() - new Date().getTime() < 2 * 24 * 60 * 60 * 1000
-    : false; // 2 days before due
+  
+  const daysLeft = dueDate
+    ? Math.ceil((new Date(dueDate).getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000))
+    : null;
+  
+  const isDueSoon = daysLeft !== null && daysLeft < 2 && daysLeft >= 0;
 
   return (
     <div>
       {dueDate && (
-          <p className={`mt-2  ${isOverdue ? 'text-red-600': 'text-gray-500'} ${isDueSoon ? 'text-yellow-500' : 'text-gray-500'}`}>
-            {isDueSoon && '2 days before due date'}
-            {isOverdue && 'The date is over'}
-            Due: {new Date(dueDate).toLocaleDateString()}
-          </p>
+        <p
+          className={`mt-2 ${
+            isOverdue
+              ? 'text-red-600'
+              : isDueSoon
+              ? 'text-yellow-500'
+              : 'text-gray-500'
+          }`}
+        >
+          {isOverdue
+            ? `The date is over.`
+            : isDueSoon
+            ? `The date expires in ${daysLeft} ${daysLeft === 1 ? 'day' : 'days'}.`
+            : `Due in ${daysLeft} days.`}
+          <br />
+          Due: {new Date(dueDate).toLocaleDateString()}
+        </p>
       )}
     </div>
-  )
+  );
 }
