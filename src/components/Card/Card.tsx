a16065165c3447f4a2f1f64d6072ed59df2f78bf
LucioFurnari <luciofurnari99@gmail.com>
2024-08-09 22:39:18 -0300

    feat: add function to retunr 404 page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,16 +2,21 @@ import Modal from "../Modal/Modal"
 import DescriptionSection from "./DescriptionSection";
 import DateSection from "./DateSection";
 import { getCard } from "@/lib/card"
+import { notFound } from "next/navigation";
 
 export default async function Card({ cardId }: { cardId: string}) {
   const card = await getCard(cardId);
 
+  console.log(card)
+  if(!card) {
+    notFound();
+  }
   return (
     <Modal>
       <div className="card p-4 rounded-lg shadow-md bg-white">
-        <h3 className="text-lg font-bold">{card.title}</h3>
+        <h3 className="text-xl font-bold">{card.title}</h3>
         <DateSection dueDate={card.dueDate} />
-        <DescriptionSection description={card.description} />
+        <DescriptionSection card={card} />
       </div>
     </Modal>
   )
