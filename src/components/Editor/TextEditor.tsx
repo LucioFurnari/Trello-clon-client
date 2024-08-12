5fb93200a1833bad7b1042a4156cae13dc537600
LucioFurnari <luciofurnari99@gmail.com>
2024-08-11 21:19:47 -0300

    feat: change component

--- src/components/Editor/TextEditor.tsx
+++ src/components/Editor/TextEditor.tsx
@@ -1,35 +1,22 @@
-import { useState, useRef, Dispatch, SetStateAction, useEffect } from 'react';
+import { useState, Dispatch, SetStateAction, useEffect, useRef, useCallback} from 'react';
 import { updateCard } from '@/lib/card';
-
-import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
+// import { useQuill } from 'react-quilljs';
+import Quill from 'quill';
+// import "quill/dist/quill.snow.css";
 import { useParams } from 'next/navigation';
-
-const modules = {
-  toolbar: [
-    [{ header: '1'}, { header: '2'}, { font: [] }],
-    [{ size: [] }],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{ list: 'ordered'}, { list: 'bullet' }],
-    ['link'],
-    ['clean']
-  ],
-};
-
-const formats = [
-  'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet',
-  'link',
-];
-
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
-import 'react-quill/dist/quill.snow.css';
-
 
 export default function MyEditor({ card, setAction }: { card: any, setAction: Dispatch<SetStateAction<boolean>> }) {
   const [content, setContent] = useState(card.description || ''); // Initial value from card
   const params = useParams<{ card: string }>();
+  // const { quill, quillRef } = useQuill();
+
+  const wrappedRef = useCallback((wrapper: any) => {
+    if (wrapper == null) return
+    wrapper.innerHTML = ''
+    const editor = document.createElement('div');
+    wrapper.append(editor)
+    new Quill('#editor-container', {theme: 'snow'})
+  }, [])
 
   async function handleSave() {
     const updatedCard = {
@@ -46,17 +33,14 @@ export default function MyEditor({ card, setAction }: { card: any, setAction: Di
   }
 
   return (
-    <div className='mt-4'>
-      <ReactQuill 
-        value={content} 
-        onChange={setContent} 
-        modules={modules}
-        formats={formats}
-      />
+    <>
+    {/* <div ref={quillRef}/> */}
+    <div id='editor-container' ref={wrappedRef}></div>
+    <div>
       <button onClick={() => console.log(content)}>Log Content</button>
       <button onClick={() => handleSave()}>Save</button>
       <button className='ml-2 mt-4 p-2 hover:bg-gray-600' onClick={() => setAction(false)}>Cancel</button>
     </div>
+    </>
   );
 }
-
