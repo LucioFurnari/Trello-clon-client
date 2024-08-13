357ae7199ffffb4bc4a50a509731d2c5cd59995f
LucioFurnari <luciofurnari99@gmail.com>
2024-08-13 20:04:00 -0300

    feat: change component to use descriptionText component

--- src/components/Card/DescriptionSection.tsx
+++ src/components/Card/DescriptionSection.tsx
@@ -1,21 +1,34 @@
 "use client"
 
-import { useState } from "react"
+import { SetStateAction, useEffect, useState } from "react"
 import TextEditor from "../Editor/TextEditor"
+import { Delta } from "quill/core";
+import DescriptionText from "./DescriptionText";
+import { CardData } from "@/types/types";
 
-export default function DescriptionSection({ card }: {card: any}) {
+export default function DescriptionSection({ card }: {card: CardData}) {
   const [openEditor, setOpenEditor] = useState(false);
+  const [content, setContent] = useState<Delta>()
+
+  useEffect(() => {
+    setContent(card.description)
+  }, [])
 
   return(
     <div className="mt-4">
       <h3 className="mb-2 text-gray-500">Description:</h3>
-      {
-        card.description &&
-        !openEditor ?
-        <p onClick={() => setOpenEditor(true)} className="ml-2">{card.description}</p>
-        :
-        <TextEditor setAction={setOpenEditor} card={card} />
-      }
+      {openEditor ? (
+        <TextEditor
+          setAction={setOpenEditor}
+          card={card}
+          setContent={setContent}
+        />
+      ) : (
+        <DescriptionText
+          delta={content}
+          setAction={setOpenEditor}
+        />
+      )}
     </div>
   )
 }
