5a7d055c62a0afeacc2782c618bc4983a56e5704
LucioFurnari <luciofurnari99@gmail.com>
2024-08-13 20:03:30 -0300

    feat: change component to save delta object

--- src/components/Editor/TextEditor.tsx
+++ src/components/Editor/TextEditor.tsx
@@ -6,10 +6,10 @@ import Quill from 'quill';
 import { useParams } from 'next/navigation';
 import { Delta } from 'quill/core';
 
-export default function MyEditor({ card, setAction }: { card: any, setAction: Dispatch<SetStateAction<boolean>> }) {
-  const [content, setContent] = useState(card.description || ''); // Initial value from card
-  const params = useParams<{ card: string }>();
+export default function MyEditor({ card,setContent ,setAction }: { card: any, setContent: Dispatch<SetStateAction<Delta | undefined>>, setAction: Dispatch<SetStateAction<boolean>>}) {
+  const params = useParams<{card: string}>()
   const [quill, setQuill] = useState<Quill>();
+  const [descriptionContent, setDescriptionContent] = useState<Delta>();
   // const { quill, quillRef } = useQuill();
 
   const wrappedRef = useCallback((wrapper: any) => {
@@ -19,6 +19,7 @@ export default function MyEditor({ card, setAction }: { card: any, setAction: Di
     wrapper.append(editor)
     const quill = new Quill('#editor-container', {theme: 'snow'})
     setQuill(quill);
+    quill.setContents(card.description)
   }, [])
 
   useEffect(() => {
@@ -27,39 +28,32 @@ export default function MyEditor({ card, setAction }: { card: any, setAction: Di
     // Handle quill changes
     const handler = (delta: Delta, oldDelta: Delta, source: string) => {
       if (source !== 'user') return
-      console.log(delta)
-      console.log(oldDelta)
+
+      const content = quill.getContents();
+      setContent(content)
+      // Get and save editor content
+      setDescriptionContent(content)
     }
     quill.on('text-change', handler);
-
     return () => {
       quill.off('text-change', handler);
     };
   }, [quill])
 
-  async function handleSave() {
-    const updatedCard = {
-      ...card,
-      description: content, // Save the HTML content directly
-    };
-    
-    const updatedData = await updateCard(params.card, updatedCard);
-    console.log(updatedData)
-    // Ensure the content is updated with what was actually saved
-    if (updatedData && updatedData.description) {
-      setContent(updatedData.description);
-    }
+  async function handleSaveContent() {
+    const updatedCard = JSON.stringify({...card, description: descriptionContent});
+    const data = await updateCard(params.card, updatedCard);
   }
 
+
   return (
-    <>
-    {/* <div ref={quillRef}/> */}
-    <div id='editor-container' ref={wrappedRef}></div>
     <div>
-      <button onClick={() => console.log(content)}>Log Content</button>
-      <button onClick={() => handleSave()}>Save</button>
+    {/* <div ref={quillRef}/> */}
+    <div id='editor-container' ref={wrappedRef} />
+    <div>
+      <button onClick={() => handleSaveContent()}>Save</button>
       <button className='ml-2 mt-4 p-2 hover:bg-gray-600' onClick={() => setAction(false)}>Cancel</button>
     </div>
-    </>
+    </div>
   );
 }
