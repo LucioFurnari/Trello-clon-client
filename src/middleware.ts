9600b7499f48b7ef70cab3acdb65701dfcd4fdbc
LucioFurnari <luciofurnari99@gmail.com>
2024-08-15 19:01:59 -0300

    fix: remove console log and unused code

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,9 @@ export async function middleware(request: NextRequest) {
   const user = await verifyToken();
   const { pathname } = request.nextUrl;
 
-  console.log('Hello', user)
-
   if (user && !pathname.startsWith('/user')) {
     return NextResponse.redirect(new URL('/user', request.url));
   }
-
-  // if (user && pathname.startsWith('/user')) {
-  //   return NextResponse.next()
-  // } 
 
   if (!user && pathname.startsWith('/user')) {
     return NextResponse.redirect(new URL('/', request.url))
