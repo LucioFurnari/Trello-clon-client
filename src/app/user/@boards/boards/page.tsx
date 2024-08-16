2ba8bee3da73e8867a5a5b10e8d398aac92826d2
LucioFurnari <luciofurnari99@gmail.com>
2024-08-16 18:06:01 -0300

    feat: change component, remove fetch function

--- src/app/user/@boards/boards/page.tsx
+++ src/app/user/@boards/boards/page.tsx
@@ -1,25 +1,12 @@
-import { getAllWorkspacesOfUser } from "@/lib/workspace"
 import WorkspacesList from "@/components/UserHome/BoardsPage/WorkspacesList";
 
-export async function getServerSideProps() {
-  const data = await getAllWorkspacesOfUser();
-
-  return {
-    props: {
-      data
-    }
-  };
-}
-
-export default async function BoardsPage({ data }: {data:any}) {
-
-  console.log(data)
+export default function BoardsPage() {
 
   return (
     <div className="flex flex-col p-4">
       <h1>This is the list of the user boards</h1>
-      <h2>Your Workspaces</h2>
-      <WorkspacesList workspaces={data} />
+      <h2 className="my-4 text-xl">Your Workspaces</h2>
+      <WorkspacesList/>
     </div>
   )
 }
