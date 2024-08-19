ecc1556748e8048cb3923caae354042906dcc952
LucioFurnari <luciofurnari99@gmail.com>
2024-08-19 18:30:18 -0300

    feat: minor style change

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -16,6 +16,6 @@ export default function NavbarButton({name, action}: NavbarButtonProps) {
 
 function IcOutlineKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg className="inline" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z"></path></svg>
+    <svg className="inline-block mb-1" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z"></path></svg>
   )
 }
--- src/components/Navbar/UserMenu/LogoutButton.tsx
+++ src/components/Navbar/UserMenu/LogoutButton.tsx
@@ -10,7 +10,7 @@ export default function LogoutButton() {
   }
 
   return (
-    <button className="ml-2" onClick={handleLogout}>
+    <button className=" text-xl ml-2 hover:bg-gray-500 border-gray-400 rounded py-1 px-2 transition-colors" onClick={handleLogout}>
       Logout
     </button>
   )
