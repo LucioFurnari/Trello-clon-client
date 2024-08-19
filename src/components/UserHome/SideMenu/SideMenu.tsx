301bf4e64be92306a476393c3377112a7a6b149c
LucioFurnari <luciofurnari99@gmail.com>
2024-08-19 18:32:45 -0300

    feat: change import component

--- src/components/UserHome/SideMenu/SideMenu.tsx
+++ src/components/UserHome/SideMenu/SideMenu.tsx
@@ -1,9 +1,11 @@
-import UserHomeLink from "./SideMenuLink"
+import LinkBoard from "./LinkBoard"
+import WorkspaceMenu from "./WorkspaceMenu/WorkspaceMenu"
 
 export default function SideMenu() {
   return (
-    <section>
-      <UserHomeLink name={"Home"} route={"/"} />
-    </section>
+    <nav className="flex flex-col max-w-60">
+      <LinkBoard />
+      <WorkspaceMenu />
+    </nav>
   )
 }
