b9011123c79c34e378ae7df58f0e04e52ce8af92
LucioFurnari <luciofurnari99@gmail.com>
2024-08-19 18:33:08 -0300

    feat: create workspace folder components

---
+++ src/components/Workspace/BoardList.tsx
@@ -0,0 +1,9 @@
+
+
+export default function BoardList() {
+  return (
+    <section>
+      
+    </section>
+  )
+}
---
+++ src/components/Workspace/WorkspaceMain.tsx
@@ -0,0 +1,9 @@
+
+
+export default function WorkspaceMain() {
+  return (
+    <section>
+      
+    </section>
+  )
+}
