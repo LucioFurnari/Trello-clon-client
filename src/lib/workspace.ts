178f0469d90744fdf911e7e1821b5cdc1d034824
LucioFurnari <luciofurnari99@gmail.com>
2024-08-21 19:30:32 -0300

    feat: add create workspace function

--- src/lib/workspace.ts
+++ src/lib/workspace.ts
@@ -1,9 +1,11 @@
 import { getCookie } from "./cookies";
 
+const API_HOST = process.env.API_HOST;
+
 export async function getWorkspace(workspaceId: string) {
   const token = getCookie();
   try {
-    const res = await fetch(`${process.env.API_HOST}/workspace/${workspaceId}`,{
+    const res = await fetch(`${API_HOST}/workspace/${workspaceId}`,{
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -32,7 +34,7 @@ export async function getAllWorkspacesOfUser() {
       return { error: true, message: 'Token not found' };
     }
 
-    const res = await fetch(`${process.env.API_HOST}/workspace`, {
+    const res = await fetch(`${API_HOST}/workspace`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -47,6 +49,37 @@ export async function getAllWorkspacesOfUser() {
 
     const data = await res.json();
     return data.workspaces;
+  } catch (error) {
+    console.error('Error in the server, it was not possible to perform the fetch', error);
+    return null;
+  }
+}
+
+export async function createWorkspace(formData: FormData) {
+  const token = getCookie();
+  const rawData = JSON.stringify(formData)
+  try {
+    if (!token) {
+      console.error('No token found');
+      return { error: true, message: 'Token not found' };
+    }
+
+    const res = await fetch(`${API_HOST}/workspace`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: `access_token=${token}`
+      },
+      body: rawData
+    });
+
+    if (!res.ok) {
+      console.error('Failed to fetch workspaces', res.status, res.statusText)
+      return { error: true, message: res.statusText};
+    }
+
+    const data = await res.json();
+    return data.workspace;
   } catch (error) {
     console.error('Error in the server, it was not possible to perform the fetch', error);
     return null;
