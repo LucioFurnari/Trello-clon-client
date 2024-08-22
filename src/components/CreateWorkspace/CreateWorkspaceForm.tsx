4150b3b935c089c0eb0ccfdf1068d55d2fe33689
LucioFurnari <luciofurnari99@gmail.com>
2024-08-22 17:26:01 -0300

    feat: add title for component

--- src/components/CreateWorkspace/CreateWorkspaceForm.tsx
+++ src/components/CreateWorkspace/CreateWorkspaceForm.tsx
@@ -2,9 +2,12 @@ import { createWorkspace } from "@/lib/workspace"
 
 export default function CreateWorkspaceForm() {
   return (
-    <form className="flex flex-col" action={createWorkspace}>
-      <input className="p-2" type="text" name="name" placeholder="Workspace name"/>
-      <button className=" bg-blue-600 hover:bg-blue-500 text-white p-2" type="submit">Create</button>
-    </form>
+    <div>
+      <h2 className="text-gray-500">Create workspace</h2>
+      <form className="flex flex-col" action={createWorkspace}>
+        <input className="p-2" type="text" name="name" placeholder="Workspace name"/>
+        <button className=" bg-blue-600 hover:bg-blue-500 text-white p-2" type="submit">Create</button>
+      </form>
+    </div>
   )
 }
