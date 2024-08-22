5b98d453b41b0b022dc0e6b067430637ed6bb3fb
LucioFurnari <luciofurnari99@gmail.com>
2024-08-22 19:16:43 -0300

    feat: add textarea for description

--- src/components/CreateWorkspace/CreateWorkspaceForm.tsx
+++ src/components/CreateWorkspace/CreateWorkspaceForm.tsx
@@ -5,8 +5,9 @@ export default function CreateWorkspaceForm() {
     <div>
       <h2 className="text-gray-500">Create workspace</h2>
       <form className="flex flex-col" action={createWorkspace}>
-        <input className="p-2" type="text" name="name" placeholder="Workspace name"/>
-        <button className=" bg-blue-600 hover:bg-blue-500 text-white p-2" type="submit">Create</button>
+        <input className="p-2 border-b-2 border-gray-500/50 mt-4" type="text" name="name" placeholder="Workspace name"/>
+        <textarea className="p-2 border-b-2 border-gray-500/50 mt-4" name="description" placeholder="Description"/>
+        <button className="bg-blue-600 hover:bg-blue-500 text-white p-2 mt-4" type="submit">Create</button>
       </form>
     </div>
   )
