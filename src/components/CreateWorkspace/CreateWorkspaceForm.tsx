b43989b949e106c6b570eb5614f8341dc5e82a40
LucioFurnari <luciofurnari99@gmail.com>
2024-08-22 17:14:42 -0300

    feat: add tailwind class

--- src/components/CreateWorkspace/CreateWorkspaceForm.tsx
+++ src/components/CreateWorkspace/CreateWorkspaceForm.tsx
@@ -2,9 +2,9 @@ import { createWorkspace } from "@/lib/workspace"
 
 export default function CreateWorkspaceForm() {
   return (
-    <form action={createWorkspace}>
-      <input type="text" name="name"/>
-      <button type="submit">Create</button>
+    <form className="flex flex-col" action={createWorkspace}>
+      <input className="p-2" type="text" name="name" placeholder="Workspace name"/>
+      <button className=" bg-blue-600 hover:bg-blue-500 text-white p-2" type="submit">Create</button>
     </form>
   )
 }
