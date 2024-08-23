f86f2f84915db1ed40153bed6d792ebc5782ff3c
LucioFurnari <luciofurnari99@gmail.com>
2024-08-23 17:24:24 -0300

    feat: import and use WorkspaceContextProvider

--- src/components/UserHome/SideMenu/SideMenu.tsx
+++ src/components/UserHome/SideMenu/SideMenu.tsx
@@ -1,11 +1,14 @@
 import LinkBoard from "./LinkBoard"
 import WorkspaceMenu from "./WorkspaceMenu/WorkspaceMenu"
+import WorkspaceContextProvider from "@/context/WorkspaceContext"
 
 export default function SideMenu() {
   return (
-    <nav className="flex flex-col max-w-60">
-      <LinkBoard />
-      <WorkspaceMenu />
-    </nav>
+    <WorkspaceContextProvider>
+      <nav className="flex flex-col max-w-60">
+        <LinkBoard />
+        <WorkspaceMenu />
+      </nav>
+    </WorkspaceContextProvider>
   )
 }
