93b06b7db55e4aa1857cc21198c725a2e5498017
LucioFurnari <luciofurnari99@gmail.com>
2024-08-23 18:51:30 -0300

    feat: change component to client, use custom hook context

--- src/components/UserHome/SideMenu/WorkspaceMenu/WorkspaceMenu.tsx
+++ src/components/UserHome/SideMenu/WorkspaceMenu/WorkspaceMenu.tsx
@@ -1,11 +1,27 @@
+'use client'
+
 import ListContainer from "./ListContainer"
 import CreateWorkspace from "./CreateWorkspace"
+import { useWorkspaceContext } from "@/context/WorkspaceContext"
 
 export default function WorkspaceMenu() {
+  const context = useWorkspaceContext();
+
   return (
     <div>
-      <h3 className="text-zinc-400 text-sm">Workspaces</h3>
-      <ListContainer name={"Example workspace"} />
+      <h3 className="text-zinc-400 text-xl border-b-[1px] border-gray-500/50 py-2 ml-2 mr-2">Workspaces</h3>
+      {
+        context?.workspace && 
+        <div>
+          {
+            context.workspace.map((item) => {
+              return(
+                <ListContainer key={item.workspace.workspaceId} name={item.workspace.name}/>
+              )
+          })
+          }
+        </div>
+      }
       <CreateWorkspace />
     </div>
   )
