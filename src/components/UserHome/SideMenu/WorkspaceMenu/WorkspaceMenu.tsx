780f3fa2151d4a74a73214b46bebd9b017b6cd32
LucioFurnari <luciofurnari99@gmail.com>
2024-08-22 23:37:15 -0300

    feat: import and use CreateWorkspace component

--- src/components/UserHome/SideMenu/WorkspaceMenu/WorkspaceMenu.tsx
+++ src/components/UserHome/SideMenu/WorkspaceMenu/WorkspaceMenu.tsx
@@ -1,10 +1,12 @@
 import ListContainer from "./ListContainer"
+import CreateWorkspace from "./CreateWorkspace"
 
 export default function WorkspaceMenu() {
   return (
     <div>
       <h3 className="text-zinc-400 text-sm">Workspaces</h3>
       <ListContainer name={"Example workspace"} />
+      <CreateWorkspace />
     </div>
   )
 }
