7116bac3116bd983e7a8a0981acabd4b9de8a87d
LucioFurnari <luciofurnari99@gmail.com>
2024-08-24 16:33:10 -0300

    feat: remove workspace list, add userBoardsPage component

--- src/app/user/@boards/boards/page.tsx
+++ src/app/user/@boards/boards/page.tsx
@@ -1,12 +1,8 @@
-import WorkspacesList from "@/components/UserHome/BoardsPage/WorkspacesList";
+import UserBoardsPage from "@/components/UserHome/BoardsPage/UserBoardsPage";
 
 export default function BoardsPage() {
 
   return (
-    <div className="flex flex-col p-4">
-      <h1>This is the list of the user boards</h1>
-      <h2 className="my-4 text-xl">Your Workspaces</h2>
-      <WorkspacesList/>
-    </div>
+    <UserBoardsPage />
   )
 }
