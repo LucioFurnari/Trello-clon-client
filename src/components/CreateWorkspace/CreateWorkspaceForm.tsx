049c5dc2f6f0dd757715708d85a60b88190f7055
LucioFurnari <luciofurnari99@gmail.com>
2024-08-26 20:05:00 -0300

    feat: change component, add react hooks and handle form submit

--- src/components/CreateWorkspace/CreateWorkspaceForm.tsx
+++ src/components/CreateWorkspace/CreateWorkspaceForm.tsx
@@ -9,24 +9,35 @@ export default function CreateWorkspaceForm() {
   const [workspaceDescription, setWorkspaceDescription] = useState('');
   const context = useWorkspaceContext();
 
-  async function handleCreateWorkspace() {
+  async function handleCreateWorkspace(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     const newWorkspace = {
       name: workspaceName,
       description: workspaceDescription
     }
     const data =  await createWorkspace(newWorkspace);
 
-    if (data.error) return;
+    if (data.error) {
+      console.log(data)
+      return
+    };
     
-    context?.setWorkspace(data);
+    const workspaceData = {
+      workspace: {
+        workspaceId: data.workspaceId,
+        name: data.name,
+        boards: []
+      }
+    }
+    context?.setWorkspace([...context.workspace, workspaceData]);
   }
 
   return (
     <div>
       <h2 className="text-gray-500">Create workspace</h2>
-      <form className="flex flex-col" action={handleCreateWorkspace}>
-        <input className="p-2 border-b-2 border-gray-500/50 mt-4" type="text" name="name" placeholder="Workspace name"/>
-        <textarea className="p-2 border-b-2 border-gray-500/50 mt-4" name="description" placeholder="Description"/>
+      <form className="flex flex-col" onSubmit={handleCreateWorkspace}>
+        <input onChange={(e) => setWorkspaceName(e.currentTarget.value)} className="p-2 border-b-2 border-gray-500/50 mt-4" type="text" name="name" placeholder="Workspace name"/>
+        <textarea onChange={(e) => setWorkspaceDescription(e.currentTarget.value)} className="p-2 border-b-2 border-gray-500/50 mt-4" name="description" placeholder="Description"/>
         <button className="bg-blue-600 hover:bg-blue-500 text-white p-2 mt-4" type="submit">Create</button>
       </form>
     </div>
