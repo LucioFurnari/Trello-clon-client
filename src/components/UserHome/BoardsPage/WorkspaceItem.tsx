10db95172bfb72394af6521b23fbd7141fc3a3c7
LucioFurnari <luciofurnari99@gmail.com>
2024-08-27 18:53:36 -0300

    feat: minor style change, increase title font size

--- src/components/UserHome/BoardsPage/WorkspaceItem.tsx
+++ src/components/UserHome/BoardsPage/WorkspaceItem.tsx
@@ -9,7 +9,7 @@ interface WorkspaceItemProp {
 export function WorkspaceItem({ name, boards, workspaceId }: WorkspaceItemProp) {
   return (
     <div className="pb-4">
-      <h3 className="pb-2 text-zinc-200">{name}</h3>
+      <h3 className="pb-2 text-zinc-100 text-2xl">{name}</h3>
       <BoardList boards={boards} workspaceId={workspaceId} />
     </div>
   )
