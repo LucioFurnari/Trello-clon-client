997c166a18b109edf20424903e2631bf96ddbd40
LucioFurnari <luciofurnari99@gmail.com>
2024-08-27 20:01:48 -0300

    feat: create folder and workspace options component

---
+++ src/components/WorkspaceOptions/WorkspaceOptions.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { SVGProps } from "react"
+
+export default function WorkspaceOptions() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <button onClick={() => setOpenMenu((value: boolean) => !value)} className="text-white">
+        <TablerPencil />
+      </button>
+      {
+        openMenu &&
+        <div className="z-[1] p-4 top-0 left-10 absolute bg-violet-500">
+        Menu
+        </div>
+      }
+    </div>
+  )
+}
+
+
+export function TablerPencil(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 20h4L18.5 9.5a2.828 2.828 0 1 0-4-4L4 16zm9.5-13.5l4 4"></path></svg>
+  )
+}
