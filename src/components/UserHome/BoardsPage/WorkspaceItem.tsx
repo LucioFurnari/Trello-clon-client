8e553dc4c532d825217ea8d315a5c0adddb28372
LucioFurnari <luciofurnari99@gmail.com>
2024-08-27 20:02:11 -0300

    feat: import workspace options component

--- src/components/UserHome/BoardsPage/WorkspaceItem.tsx
+++ src/components/UserHome/BoardsPage/WorkspaceItem.tsx
@@ -1,4 +1,5 @@
 import BoardList from "./BoardList"
+import WorkspaceOptions from "@/components/WorkspaceOptions/WorkspaceOptions"
 import { BoardData } from "@/types/types"
 
 interface WorkspaceItemProp {
@@ -10,7 +11,8 @@ interface WorkspaceItemProp {
 export function WorkspaceItem({ name, boards, workspaceId }: WorkspaceItemProp) {
   return (
     <div className="pb-4">
-      <h3 className="pb-2 text-zinc-100 text-2xl">{name}</h3>
+      <h3 className="pb-2 mr-4 text-zinc-100 text-2xl inline-block">{name}</h3>
+      <WorkspaceOptions />
       <BoardList boards={boards} workspaceId={workspaceId} />
     </div>
   )
