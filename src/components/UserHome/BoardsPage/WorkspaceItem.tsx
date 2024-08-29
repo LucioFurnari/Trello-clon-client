61f646a07727ed26fbc51b50fa881c6d457d24c2
LucioFurnari <luciofurnari99@gmail.com>
2024-08-29 18:53:58 -0300

    feat: add workspace id prop

--- src/components/UserHome/BoardsPage/WorkspaceItem.tsx
+++ src/components/UserHome/BoardsPage/WorkspaceItem.tsx
@@ -12,7 +12,7 @@ export function WorkspaceItem({ name, boards, workspaceId }: WorkspaceItemProp) 
   return (
     <div className="pb-4">
       <h3 className="pb-2 mr-4 text-zinc-100 text-2xl inline-block">{name}</h3>
-      <WorkspaceOptions />
+      <WorkspaceOptions workspaceId={workspaceId} />
       <BoardList boards={boards} workspaceId={workspaceId} />
     </div>
   )
