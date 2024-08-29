7f3a4a9768e07532ac8149af40f75347520c1b53
LucioFurnari <luciofurnari99@gmail.com>
2024-08-29 18:54:31 -0300

    feat: minor style change

--- src/components/UserHome/SideMenu/WorkspaceMenu/MenuButton.tsx
+++ src/components/UserHome/SideMenu/WorkspaceMenu/MenuButton.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export default function MenuButton({ name, action }: ButtonProps) {
   return (
-    <button className="p-2 text-zinc-300 hover:bg-zinc-600 transition-colors rounded" onClick={action}>
+    <button className="p-2 text-zinc-300 hover:bg-zinc-600 w-full text-left transition-colors rounded" onClick={action}>
       {name}
     </button>
   )
