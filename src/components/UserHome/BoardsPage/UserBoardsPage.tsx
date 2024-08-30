2ecefb4c4e0f313202555a4c70bfee786b456cd2
LucioFurnari <luciofurnari99@gmail.com>
2024-08-30 15:47:38 -0300

    feat: remove context provided

--- src/components/UserHome/BoardsPage/UserBoardsPage.tsx
+++ src/components/UserHome/BoardsPage/UserBoardsPage.tsx
@@ -1,14 +1,11 @@
 import WorkspacesList from "./WorkspacesList";
-import WorkspaceContextProvider from "@/context/WorkspaceContext";
 
 export default function UserBoardsPage() {
   return (
-    <WorkspaceContextProvider >
-      <section className="flex flex-col p-4">
-        <h1>This is the list of the user boards</h1>
-        <h2 className="my-4 text-xl">Your Workspaces</h2>
-        <WorkspacesList/>
-      </section>
-    </WorkspaceContextProvider>
+    <section className="flex flex-col p-4">
+      <h1>This is the list of the user boards</h1>
+      <h2 className="my-4 text-xl">Your Workspaces</h2>
+      <WorkspacesList/>
+    </section>
   )
 }
--- src/components/UserHome/SideMenu/SideMenu.tsx
+++ src/components/UserHome/SideMenu/SideMenu.tsx
@@ -1,14 +1,11 @@
 import LinkBoard from "./LinkBoard"
 import WorkspaceMenu from "./WorkspaceMenu/WorkspaceMenu"
-import WorkspaceContextProvider from "@/context/WorkspaceContext"
 
 export default function SideMenu() {
   return (
-    <WorkspaceContextProvider>
-      <nav className="flex flex-col max-w-60">
-        <LinkBoard />
-        <WorkspaceMenu />
-      </nav>
-    </WorkspaceContextProvider>
+    <nav className="flex flex-col max-w-60">
+      <LinkBoard />
+      <WorkspaceMenu />
+    </nav>
   )
 }
