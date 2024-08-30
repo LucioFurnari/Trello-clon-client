57743096bd630c41b650eec7959866249150268c
LucioFurnari <luciofurnari99@gmail.com>
2024-08-30 00:20:07 -0300

    feat: create edit workspace component

---
+++ src/components/WorkspaceOptions/EditWorkspace.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+interface UpdateWorkspaceProps {
+  workspaceId: string
+}
+
+export default function EditWorkspace({ workspaceId }: UpdateWorkspaceProps) {
+  return (
+    <>
+      <button>Edit</button>
+    </>
+  )
+}
