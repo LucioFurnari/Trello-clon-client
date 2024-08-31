486c4aa6841ee7186a9544f8a9b4b7d348c10ae3
LucioFurnari <luciofurnari99@gmail.com>
2024-08-31 17:02:52 -0300

    feat: minor style change

--- src/components/UserHome/SideMenu/WorkspaceMenu/BoardLink.tsx
+++ src/components/UserHome/SideMenu/WorkspaceMenu/BoardLink.tsx
@@ -9,7 +9,7 @@ export default function BoardLink({ boardId, name }: OptionMenuProps) {
   return (
     <ul>
       <li>
-        <Link className="block text-center text-zinc-300" href={`/board/${boardId}`}>{name}</Link>
+        <Link className="block text-left pl-6 py-1 text-zinc-300 hover:bg-slate-500/50" href={`/board/${boardId}`}>{name}</Link>
       </li>
     </ul>
   )
