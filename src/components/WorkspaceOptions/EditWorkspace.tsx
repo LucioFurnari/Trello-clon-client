eb8e03d67340a688d1f14d7fe4554faf17b4ac9a
LucioFurnari <luciofurnari99@gmail.com>
2024-08-31 23:48:00 -0300

    feat: add message modal, and state hook

--- src/components/WorkspaceOptions/EditWorkspace.tsx
+++ src/components/WorkspaceOptions/EditWorkspace.tsx
@@ -1,13 +1,26 @@
+import MessageModal from "../Modal/MessageModal"
 import { useState } from "react"
 
 interface UpdateWorkspaceProps {
-  workspaceId: string
+  id: string
 }
 
-export default function EditWorkspace({ workspaceId }: UpdateWorkspaceProps) {
+export default function EditWorkspace({ id }: UpdateWorkspaceProps) {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <>
-      <button>Edit</button>
+      <button onClick={() => setOpenModal(true)} className="p-2">Edit</button>
+      {
+        openModal &&
+        <MessageModal setAction={() => setOpenModal(false)}>
+          <div>
+            <form>
+              <input type="text" name="name" placeholder="Board name"/>
+            </form>
+          </div>
+        </MessageModal>
+      }
     </>
   )
 }
