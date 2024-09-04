8d30119c24f1d0c7dfdca2ced57a9b9f9e6e378a
LucioFurnari <luciofurnari99@gmail.com>
2024-09-03 23:24:53 -0300

    feat: change component, add getworkspace function

--- src/components/Workspace/WorkspaceMain.tsx
+++ src/components/Workspace/WorkspaceMain.tsx
@@ -1,13 +1,30 @@
+'use client'
+
 import BoardList from "../BoardList/BoardList"
+import { WorkspaceData, BoardData } from "@/types/types"
+import { getWorkspace } from "@/lib/workspace"
+import { useEffect, useState } from "react"
 
 interface WorkspaceMainProps {
   id: string,
 }
 
 export default function WorkspaceMain({ id }: WorkspaceMainProps) {
+  const [workspace, setWorkspace] = useState<WorkspaceData>();
+
+  useEffect(() => {
+    async function handleGetWorkspace() {
+      const data = await getWorkspace(id);
+      console.log(data)
+      setWorkspace(data);
+    }
+    handleGetWorkspace()
+  }, [id])
+
+
   return (
     <section>
-      <BoardList workspaceId={id} />
+      <BoardList boards={workspace?.boards}  workspaceId={id} createHandle={() => {}} deleteHandle={() => {}}/>
     </section>
   )
 }
