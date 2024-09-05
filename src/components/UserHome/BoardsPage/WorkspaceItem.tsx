cbe896ae3bfbf847034f18656574a80b29e435c3
LucioFurnari <luciofurnari99@gmail.com>
2024-09-05 19:01:15 -0300

    fix: add description prop

--- src/components/UserHome/BoardsPage/WorkspaceItem.tsx
+++ src/components/UserHome/BoardsPage/WorkspaceItem.tsx
@@ -6,11 +6,12 @@ import { useWorkspaceContext } from "@/context/WorkspaceContext"
 
 interface WorkspaceItemProp {
   name: string,
+  description?: string,
   boards?: BoardData[] | [],
   workspaceId: string
 }
 
-export function WorkspaceItem({ name, boards, workspaceId }: WorkspaceItemProp) {
+export function WorkspaceItem({ name, boards, workspaceId, description }: WorkspaceItemProp) {
   const context = useWorkspaceContext();
 
   function handleCreateBoard(board: any) {
@@ -36,7 +37,7 @@ export function WorkspaceItem({ name, boards, workspaceId }: WorkspaceItemProp) 
   return (
     <div className="pb-4">
       <WorkspaceLink name={name} id={workspaceId} />
-      <WorkspaceOptions workspaceId={workspaceId} />
+      <WorkspaceOptions workspaceId={workspaceId} name={name} description={description} />
       <BoardList boards={boards} workspaceId={workspaceId} createHandle={handleCreateBoard} deleteHandle={handleDeleteBoard}/>
     </div>
   )
