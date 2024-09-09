35bc185efc8897ca36245fcc653c0863389bba35
LucioFurnari <luciofurnari99@gmail.com>
2024-09-08 22:30:59 -0300

    fix: change height of the component

--- src/components/Board/BoardPage.tsx
+++ src/components/Board/BoardPage.tsx
@@ -12,7 +12,7 @@ export default async function BoardPage({ params }: { params: { board: string }}
     notFound();
   }
   return (
-    <main className="bg-purple-500 min-h-[calc(100vh-56px)] overflow-auto">
+    <main className="bg-purple-500 min-h-[calc(100vh-52px)] overflow-auto">
       <BoardHeader title={data.title} />
       <ListContextProvider>
         <ListContainer lists={data.lists} />
