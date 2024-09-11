835b8ef3feca6557fa710dcb5daf6d33dcce766b
LucioFurnari <luciofurnari99@gmail.com>
2024-09-11 16:35:09 -0300

    feat: add modal to show user register

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -3,6 +3,8 @@
 import { registerUser } from "@/lib/auth"
 import { SubmitButton } from "../SubmitButton"
 import { useFormState } from "react-dom"
+import Link from "next/link"
+import MessageModal from "../Modal/MessageModal"
 import Fieldset from "../Fieldset"
 
 const initialState = {
@@ -14,7 +16,6 @@ const initialState = {
 export default function RegisterForm() {
   const [state, formAction] = useFormState(registerUser, initialState)
 
-
   return (
     <form className="flex flex-col mt-8 p-4 w-full md:max-w-2xl" action={formAction}>
       <Fieldset type={"text"} name={"username"} errors={state?.errors} />
@@ -23,7 +24,10 @@ export default function RegisterForm() {
       <SubmitButton />
       {
         state?.success &&
-        <p>{state.message}</p>
+        <MessageModal setAction={() => {}}>
+          <p className="text-center text-2xl mb-4">{state.message}</p>
+          <Link className="bg-blue-600 text-gray-200 p-2 px-8 mx-auto text-center rounded" href={'/login'}>Log in</Link>
+        </MessageModal>
       }
       {
         !state?.success &&
