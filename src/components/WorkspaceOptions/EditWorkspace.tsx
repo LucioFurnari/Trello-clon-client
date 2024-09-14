2544fcd8ae9d819a9323b15184a06ec2db0c76ba
LucioFurnari <luciofurnari99@gmail.com>
2024-09-13 23:54:56 -0300

    feat: remove multiple status hook, use a object

--- src/components/WorkspaceOptions/EditWorkspace.tsx
+++ src/components/WorkspaceOptions/EditWorkspace.tsx
@@ -11,16 +11,25 @@ interface UpdateWorkspaceProps {
 
 export default function EditWorkspace({ id, workspaceName, workspaceDesc, editAction }: UpdateWorkspaceProps) {
   const [openModal, setOpenModal] = useState(false);
-  const [name, setName] = useState(workspaceName);
-  const [description, setDescription] = useState(workspaceDesc);
+  const [editData, setEditData] = useState({
+    name: workspaceName,
+    description: workspaceDesc,
+    visibilityPrivate: true,
+    visibilityPublic: false
+  });
+
+  function handleEditData(event: React.FormEvent<HTMLInputElement>) {
+    const {name, value} = event.currentTarget;
+    setEditData({
+      ...editData,
+      [name]: value 
+    })
+  }
 
   async function handleUpdateWorkspace(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const editedData = {
-      name,
-      description
-    }
-    const res = await updateWorkspace(id, editedData);
+
+    const res = await updateWorkspace(id, editData);
 
     if (!res.error) {
       editAction(id, res)
@@ -35,8 +44,16 @@ export default function EditWorkspace({ id, workspaceName, workspaceDesc, editAc
         <MessageModal setAction={() => setOpenModal(false)}>
           <div>
             <form className="flex flex-col" onSubmit={handleUpdateWorkspace}>
-              <input onChange={(event: React.FormEvent<HTMLInputElement>) => setName(event.currentTarget.value)} className="border-b-2 mb-4 p-2" type="text" name="name" placeholder="Workspace name" value={name}/>
-              <input onChange={(event: React.FormEvent<HTMLInputElement>) => setDescription(event.currentTarget.value)} className="border-b-2 mb-4 p-2" type="text" name="description" placeholder="Workspace description" value={description}/>
+              <input onChange={handleEditData} className="border-b-2 mb-4 p-2" type="text" name="name" placeholder="Workspace name" value={editData.name}/>
+              <input onChange={handleEditData} className="border-b-2 mb-4 p-2" type="text" name="description" placeholder="Workspace description" value={editData.description}/>
+              <fieldset>
+                <input  type="checkbox" id="private" name="visibilityPrivate" checked={editData.visibilityPrivate}/>
+                <label htmlFor="private">Private</label>
+              </fieldset>
+              <fieldset>
+                <input className="inline" type="checkbox" id="public" name="visibilityPublic" checked={editData.visibilityPublic}/>
+                <label htmlFor="public">Public</label>
+              </fieldset>
               <button className="bg-blue-600 text-gray-200 py-2 px-8 ml-auto" type="submit">Edit</button>
             </form>
           </div>
