ccc29f4f93ccd2f992a08300ed6aa363c98c04eb
LucioFurnari <luciofurnari99@gmail.com>
2024-09-13 21:19:05 -0300

    feat: import accessDenied component, change height of the page

--- src/components/Workspace/WorkspacePage.tsx
+++ src/components/Workspace/WorkspacePage.tsx
@@ -1,6 +1,7 @@
 import { getWorkspace } from "@/lib/workspace"
 import WorkspaceHeader from "./WorkspaceHeader"
 import WorkspaceMain from "./WorkspaceMain"
+import AccessDenied from "./AccessDenied"
 
 interface WorkspacePageProp {
   workspaceId: string
@@ -9,10 +10,18 @@ interface WorkspacePageProp {
 export default async function WorkspacePage({ workspaceId }: WorkspacePageProp) {
   const workspace = await getWorkspace(workspaceId);
   
+  console.log(workspace)
   return (
-    <main className=" bg-slate-800 min-h-[calc(100vh-52px)]">
-      <WorkspaceHeader title={workspace.name} description={workspace.description} visibilityPrivate={workspace.visibilityPrivate} visibilityPublic={workspace.visibilityPublic} />
-      <WorkspaceMain id={workspaceId} />
+    <main className=" bg-slate-800 h-[calc(100vh-52px)] min-h-[calc(100vh-52px)]">
+      {
+        workspace.statusCode === 403 ?
+          <AccessDenied />
+        : 
+          <>
+          <WorkspaceHeader title={workspace.name} description={workspace.description} visibilityPrivate={workspace.visibilityPrivate} visibilityPublic={workspace.visibilityPublic} />
+          <WorkspaceMain id={workspaceId} />
+          </>
+      }
     </main>
   )
 }
