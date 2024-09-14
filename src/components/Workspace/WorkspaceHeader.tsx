adf8148ab7d4d5e864e993e7d636bd26e10ff57f
LucioFurnari <luciofurnari99@gmail.com>
2024-09-13 23:48:48 -0300

    feat: change component to client, import workspace options

--- src/components/Workspace/WorkspaceHeader.tsx
+++ src/components/Workspace/WorkspaceHeader.tsx
@@ -1,14 +1,24 @@
+'use client'
+
+import WorkspaceOptions from "../WorkspaceOptions/WorkspaceOptions"
+
 interface WorkspaceHeaderProps {
+  id: string,
   title: string,
   description: string,
   visibilityPrivate: boolean,
   visibilityPublic: boolean
 }
 
-export default function WorkspaceHeader({ title, description, visibilityPrivate, visibilityPublic }: WorkspaceHeaderProps) {
+export default function WorkspaceHeader({ id, title, description, visibilityPrivate, visibilityPublic }: WorkspaceHeaderProps) {
   return (
     <header className="w-fit ml-20 py-8">
       <h1 className="text-4xl text-left text-gray-200">{title}</h1>
+      <WorkspaceOptions workspaceId={id} name={title} handleDelete={function (elem?: any): void {
+        throw new Error("Function not implemented.")
+      } } handleEdit={function (id: string, elem?: any): void {
+        throw new Error("Function not implemented.")
+      } } />
       <span className="text-sm text-gray-200">
         {
           visibilityPrivate && 'Private'
