80b3be120f4b2ac52aa58c25e49bfd0d745caec6
LucioFurnari <luciofurnari99@gmail.com>
2024-09-16 18:13:31 -0300

    feat: add handle edit function, change type of prop

--- src/components/Workspace/WorkspaceHeader.tsx
+++ src/components/Workspace/WorkspaceHeader.tsx
@@ -1,26 +1,42 @@
 'use client'
 
 import WorkspaceOptions from "../WorkspaceOptions/WorkspaceOptions";
-import { WorkspaceType } from "@/types/types";
+import { WorkspaceData } from "@/types/types";
+import { useState } from "react";
 
 interface WorkspaceHeaderProps {
-  workspace: WorkspaceType,
+  workspace: WorkspaceData,
 };
 
 export default function WorkspaceHeader({ workspace }: WorkspaceHeaderProps) {
+  const [workspaceState, setWorkspace] = useState(workspace);
+
+  function handleEditWorkspace(newData: any) {
+    setWorkspace((prevState) => ({
+      ...prevState,
+      workspaceId: newData.workspaceId,
+      name: newData.name,
+      description: newData.description,
+      visibilityPrivate: newData.visibilityPrivate,
+      visibilityPublic: newData.visibilityPublic,
+      canEditAdmin: newData.canEditAdmin,
+      canEditUser: newData.canEditUser,
+    }))
+  }
+
   return (
     <header className="w-fit ml-20 py-8">
-      <h1 className="text-4xl text-left text-gray-200">{workspace.name}</h1>
-      <WorkspaceOptions workspaceId={workspace.workspaceId} workspace={workspace} handleDelete={() => { } } handleEdit={function (id: string, elem?: any): void {}}  />
+      <h1 className="text-4xl text-left text-gray-200">{workspaceState.name}</h1>
+      <WorkspaceOptions workspaceId={workspace.workspaceId} workspace={workspaceState} handleDelete={() => { } } handleEdit={handleEditWorkspace}  />
       <span className="text-sm text-gray-200">
         {
-          workspace.visibilityPrivate && 'Private'
+          workspaceState.visibilityPrivate && 'Private'
         }
         {
-          workspace.visibilityPublic && 'Public'
+          workspaceState.visibilityPublic && 'Public'
         }
       </span>
-      <p className="mt-4 text-gray-200">{workspace.description}</p>
+      <p className="mt-4 text-gray-200">{workspaceState.description}</p>
     </header>
   )
 }
