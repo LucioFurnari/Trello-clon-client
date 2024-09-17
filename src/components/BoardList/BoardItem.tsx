78c54b65236af16cb9449f6431a9628860783ca9
LucioFurnari <luciofurnari99@gmail.com>
2024-09-17 20:30:16 -0300

    feat: add delete icon

--- src/components/BoardList/BoardItem.tsx
+++ src/components/BoardList/BoardItem.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import MessageModal from "@/components/Modal/MessageModal"
 import DeleteBoardButton from "./DeleteBoardButton"
-import { useState } from "react"
+import { SVGProps, useState } from "react"
 
 interface BoardItemProp {
   title: string,
@@ -17,13 +17,15 @@ export default function BoardItem({ title, boardId, workspaceId, setAction }: Bo
 
   const handleCloseModal = () => setOpenModal(false);
   return (
-    <li className="bg-pink-500 cursor-pointer relative">
+    <li className="bg-pink-500 cursor-pointer relative rounded">
       <Link 
         href={`/board/${boardId}`} 
         className="inline-block w-full pb-14 pt-2 pl-2">
         { title }
       </Link>
-      <button onClick={() => setOpenModal(true)} className="bg-red-600 hover:bg-red-500 p-1 rounded absolute right-4 top-1">Delete</button>
+      <button onClick={() => setOpenModal(true)} className=" hover:bg-red-500 p-1 rounded absolute right-4 top-1">
+        <MdiTrashCanOutline />
+      </button>
       {openModal && 
       <MessageModal setAction={handleCloseModal}> 
         <>
@@ -34,5 +36,11 @@ export default function BoardItem({ title, boardId, workspaceId, setAction }: Bo
       </MessageModal>
       }
     </li>
+  )
+}
+
+function MdiTrashCanOutline(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className="w-6 h-auto" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"></path></svg>
   )
 }
