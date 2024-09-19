0c93f9827d32e7a57f80fff324ed868f32a029f7
LucioFurnari <luciofurnari99@gmail.com>
2024-09-18 23:28:48 -0300

    feat: add card to see the selected cover color

--- src/components/BoardList/CreateBoardButton.tsx
+++ src/components/BoardList/CreateBoardButton.tsx
@@ -54,6 +54,7 @@ export default function CreateBoardButton({workspaceId, setAction}: CreateBoardB
             <input onChange={handleInput} className="p-2 mb-8 border-b-2 border-gray-700" type="text" placeholder="Title" name="title" />
             <textarea onChange={handleInput} className="p-2 mb-8 border-b-2 border-gray-700" name="description" placeholder="Description..."/>
             <ColorPicker hideInput={['rgb', 'hsv']} color={color} onChange={setColor}/>
+            <div style={{ backgroundColor: color.hex} } className="my-4 rounded w-full h-20"></div>
             <button className="bg-blue-600 hover:bg-blue-500 text-gray-200 py-2 text-xl" type="submit">Create</button>
           </form>
         </MessageModal>
