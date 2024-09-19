9f419e764ab30814d850793f1b653b8b2e9e36e1
LucioFurnari <luciofurnari99@gmail.com>
2024-09-18 22:47:40 -0300

    feat: add background cover color

--- src/components/Board/BoardPage.tsx
+++ src/components/Board/BoardPage.tsx
@@ -12,7 +12,7 @@ export default async function BoardPage({ params }: { params: { board: string }}
     notFound();
   }
   return (
-    <main className="bg-purple-500 min-h-[calc(100vh-52px)] overflow-auto">
+    <main style={data.coverColor ? {backgroundColor: data.coverColor} : {backgroundColor: 'rgb(59 130 246 / var(--tw-bg-opacity))'} } className={" bg-blue-500 min-h-[calc(100vh-52px)] overflow-auto"}>
       <BoardHeader title={data.title} />
       <ListContextProvider>
         <ListContainer lists={data.lists} />
