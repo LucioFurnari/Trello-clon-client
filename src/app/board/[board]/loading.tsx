a1bb62a1c0a112e41e3dc725cf1c501904a23899
LucioFurnari <luciofurnari99@gmail.com>
2024-09-22 21:00:53 -0300

    feat: add loading page for board

---
+++ src/app/board/[board]/loading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loading() {
+  return (
+    <div className=" bg-slate-700 min-h-[calc(100vh-52px)] flex items-center justify-center">
+      <h1 className="text-gray-300 text-4xl">Loading...</h1>
+    </div>
+  )
+}
