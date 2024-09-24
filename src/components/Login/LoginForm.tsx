8725d1f6f5ce8c10c2df608e1962150a4a124001
LucioFurnari <luciofurnari99@gmail.com>
2024-09-23 21:47:28 -0300

    feat: add message to succes login

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,7 +5,7 @@ import { useFormState } from "react-dom"
 import Fieldset from "../Fieldset"
 import { SubmitButton } from "../SubmitButton"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { SVGProps, useEffect } from "react"
 import { useUserContext } from "@/context/UserContext"
 
 const initialState = {
@@ -21,8 +21,8 @@ export default function LoginForm() {
 
   useEffect(() => {
     if (state.success) {
-      setIsLogged(true);
       router.replace('/user', { scroll: false })
+      setIsLogged(true);
     }
   }, [state.success, router, setIsLogged]);
 
@@ -32,6 +32,20 @@ export default function LoginForm() {
       <Fieldset type={"password"} name={"password"} errors={state?.errors} />
       <SubmitButton />
       <p className="mx-auto mt-4">{state?.message}</p>
+      {
+        state.success &&
+        <>
+        <p className="text-center text-2xl">User logged successful</p>
+        <p className="text-center">Redirecting to the dashboard</p>
+        <MaterialSymbolsCheck />
+        </>
+      }
     </form>
+  )
+}
+
+export function MaterialSymbolsCheck(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className=" mx-auto w-10 h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z"></path></svg>
   )
 }
