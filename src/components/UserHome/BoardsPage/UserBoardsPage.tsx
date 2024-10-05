fc5fbeda53052af5ebce81d348431466bab2069a
LucioFurnari <luciofurnari99@gmail.com>
2024-10-05 15:15:23 -0300

    feat: change title

--- src/components/UserHome/BoardsPage/UserBoardsPage.tsx
+++ src/components/UserHome/BoardsPage/UserBoardsPage.tsx
@@ -3,8 +3,7 @@ import WorkspacesList from "./WorkspacesList";
 export default function UserBoardsPage() {
   return (
     <section className="flex flex-col p-4">
-      <h1>This is the list of the user boards</h1>
-      <h2 className="my-4 text-xl">Your Workspaces</h2>
+      <h1 className="my-4 text-xl">Your Workspaces</h1>
       <WorkspacesList/>
     </section>
   )
