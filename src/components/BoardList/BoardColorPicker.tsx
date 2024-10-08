6f7ccc2b33396003b88b65b604bd4f08a174d02d
LucioFurnari <luciofurnari99@gmail.com>
2024-10-08 19:34:15 -0300

    feat: remove useState hook and conditional rendering

--- src/components/BoardList/BoardColorPicker.tsx
+++ src/components/BoardList/BoardColorPicker.tsx
@@ -1,5 +1,4 @@
 import { ColorPicker, IColor } from "react-color-palette"
-import { useState } from "react"
 
 interface ColorPickerProps {
   color: IColor,
@@ -7,29 +6,11 @@ interface ColorPickerProps {
 }
 
 export default function BoardColorPicker({ color, setColor }: ColorPickerProps) {
-  const [open, setOpen] = useState(false);
-
-  function openColorPicker() {
-    setOpen(true);
-  }
-
-  function closeColorPicker() {
-    setOpen(false);
-  }
 
   return (
-    <div>
-      <button type="button" className="p-2 bg-slate-400/50 hover:bg-slate-300/50 rounded" onClick={openColorPicker}>Background color</button>
-      {
-        open && 
-        <>
-        <button type="button" className="ml-6 p-2 hover:bg-slate-300/50 rounded" onClick={closeColorPicker}>Cancel</button>
-        <div className="mt-4">
-          <ColorPicker color={color} onChange={setColor} />
-          <div style={{ backgroundColor: color.hex} } className="my-4 rounded w-full h-20"></div>
-        </div>
-        </>
-      }
+    <div className="mt-4">
+      <ColorPicker color={color} onChange={setColor} />
+      <div style={{ backgroundColor: color.hex} } className="my-4 rounded w-full h-20"></div>
     </div>
   )
 }
