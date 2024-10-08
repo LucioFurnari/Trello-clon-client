1ad057f70f511653b0d020de2fe5720909bf3f64
LucioFurnari <luciofurnari99@gmail.com>
2024-10-08 19:40:13 -0300

    feat: add trycatch to handle error

--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -1,18 +1,24 @@
+import { ImageData } from "@/types/types";
 
 export async function getRandomImages() {
-  const res = await fetch(`${process.env.API_HOST}/unsplash/10`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/unsplash/10`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+  
+    if (!res.ok) {
+      console.error('Failed to fetch workspaces', res.status, res.statusText)
+      return null;
     }
-  });
-
-  if (!res.ok) {
-    console.error('Failed to fetch workspaces', res.status, res.statusText)
+  
+    const images: ImageData[] = await res.json();
+  
+    return images;
+  } catch (error) {
+    console.error('Error during fetching images:', error);
     return null;
   }
-
-  const images = await res.json();
-
-  return images;
 }
