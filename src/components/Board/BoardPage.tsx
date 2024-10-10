489ccf5b601ed2ce5980728e8a70bdf12e397ee6
LucioFurnari <luciofurnari99@gmail.com>
2024-10-09 23:43:49 -0300

    feat: add cover image as background image

--- src/components/Board/BoardPage.tsx
+++ src/components/Board/BoardPage.tsx
@@ -12,7 +12,18 @@ export default async function BoardPage({ params }: { params: { board: string }}
     notFound();
   }
   return (
-    <main style={data.coverColor ? {backgroundColor: data.coverColor} : {backgroundColor: 'rgb(59 130 246 / var(--tw-bg-opacity))'} } className={" bg-blue-500 min-h-[calc(100vh-52px)] overflow-auto"}>
+    <main style={{
+      ...(data.coverImage && {
+        backgroundImage: `url(${data.coverImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }),
+      backgroundColor: data.coverColor
+        ? data.coverColor
+        : 'rgb(59 130 246 / var(--tw-bg-opacity))',
+    }}
+    className={" bg-blue-500 min-h-[calc(100vh-52px)] overflow-auto"}>
       <BoardHeader title={data.title} />
       <ListContextProvider>
         <ListContainer lists={data.lists} />
