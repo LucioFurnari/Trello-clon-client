5419c68db8473038b934aeda427df45b2021efe4
LucioFurnari <luciofurnari99@gmail.com>
2024-10-09 23:43:25 -0300

    feat: add cover image as background image

--- src/components/BoardList/BoardItem.tsx
+++ src/components/BoardList/BoardItem.tsx
@@ -12,15 +12,26 @@ interface BoardItemProp {
 }
 
 export default function BoardItem({ board, setAction }: BoardItemProp) {
-  const {boardId, title, description, coverColor} = board;
+  const {boardId, title, coverImage, coverColor} = board;
   const [openModal, setOpenModal] = useState(false);
 
   const handleCloseModal = () => setOpenModal(false);
   return (
-    <li style={coverColor ? {backgroundColor: coverColor} : {backgroundColor: 'rgb(59 130 246 / var(--tw-bg-opacity))'} } className={'cursor-pointer relative rounded min-h-28'}>
+    <li style={{
+      ...(coverImage && {
+        backgroundImage: `url(${coverImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }),
+      backgroundColor: coverColor
+        ? coverColor
+        : 'rgb(59 130 246 / var(--tw-bg-opacity))',
+    }}
+      className={'cursor-pointer relative rounded min-h-28 drop-shadow-xl'}>
       <Link 
         href={`/board/${boardId}`} 
-        className="inline-block w-full h-full pt-2 pl-2 hover:bg-white/20">
+        className="inline-block w-full h-full pt-2 pl-2 hover:bg-white/20 text-white font-semibold">
         { title }
       </Link>
       <button onClick={() => setOpenModal(true)} className=" hover:bg-red-500 p-1 rounded absolute right-4 top-1">
@@ -41,6 +52,6 @@ export default function BoardItem({ board, setAction }: BoardItemProp) {
 
 function MdiTrashCanOutline(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg className="w-6 h-auto" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"></path></svg>
+    <svg className="w-6 h-auto fill-white" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" {...props}><path d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3zM7 6h10v13H7zm2 2v9h2V8zm4 0v9h2V8z"></path></svg>
   )
 }
