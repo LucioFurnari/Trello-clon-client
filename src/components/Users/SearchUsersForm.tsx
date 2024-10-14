6d9f9305be01cf7bdb8cc428d0911074e5093040
LucioFurnari <luciofurnari99@gmail.com>
2024-10-14 20:20:41 -0300

    feat: immprove search user function

--- src/components/Users/SearchUsersForm.tsx
+++ src/components/Users/SearchUsersForm.tsx
@@ -3,21 +3,34 @@ import { useState } from "react";
 
 interface SearchFormProps {
   setResults: (value: []) => void,
-  setLoading: (value: boolean) => void
+  setLoading: (value: boolean) => void,
+  setSearched: (value: boolean) => void
 }
 
-export default function SearchUsersForm({setResults, setLoading}: SearchFormProps) {
+export default function SearchUsersForm({ setResults, setLoading, setSearched }: SearchFormProps) {
   const [query, setQuery] = useState('');
-
+  const [error, setError] = useState<string | null>(null);
 
   async function handleSearch(event: React.FormEvent) {
     event.preventDefault();
 
-    if (!query.trim()) return;
+    if (!query.trim()) {
+      setError('Please enter a search term.');
+      setSearched(false);
+      return;
+    }
     setLoading(true);
-    const users = await findUsers(query);
-    setResults(users);
-    setLoading(false);
+    setError(null);
+    setSearched(true);
+
+    try {
+      const users = await findUsers(query);
+      setResults(users);
+    } catch (error) {
+      setError('An error occurred while searching. Please try again.');
+    } finally {
+      setLoading(false);
+    }
   }
 
   function handleInput(e: any) {
@@ -25,13 +38,16 @@ export default function SearchUsersForm({setResults, setLoading}: SearchFormProp
   }
 
   return (
-    <form onSubmit={handleSearch}>
-      <input 
+    <form className="flex flex-col" onSubmit={handleSearch}>
+      <input
+        className="border-b-2 py-2 pl-2 rounded-t"
         value={query}
         onChange={handleInput}
         placeholder="Search for users..."
       />
-      <button type="submit">Search</button>
+      <span className="text-xs text-gray-500 mt-2">* Enter an name or a email to find the user.</span>
+      { error && <p className="text-red-500">{error}</p>}
+      <button className="ml-auto mt-4 p-2 px-4 rounded hover:bg-slate-300/50" type="submit">Search</button>
     </form>
   )
 }
