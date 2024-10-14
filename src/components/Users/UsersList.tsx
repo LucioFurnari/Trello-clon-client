c5cd27d435c016f59267ea4bc1f3a90ed14ce5b7
LucioFurnari <luciofurnari99@gmail.com>
2024-10-14 20:21:05 -0300

    feat: add prop and iteration

--- src/components/Users/UsersList.tsx
+++ src/components/Users/UsersList.tsx
@@ -1,9 +1,20 @@
 
+interface UserListProps {
+  users: any[],
+}
 
-export default function UsersList() {
+export default function UsersList({ users }: UserListProps) {
   return (
-    <div>
+    <ul>
+      {
+        users.length > 0 &&
+        users.map((user) => {
+          return (
+            <li key={user.id}>{user.name}</li>
+          )
+        })
       
-    </div>
+      }
+    </ul>
   )
 }
