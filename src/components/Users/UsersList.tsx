b2bb9a08d41728430d51a4fddf11e303ab911e31
LucioFurnari <luciofurnari99@gmail.com>
2024-10-15 23:59:28 -0300

    feat: add add member component

--- src/components/Users/UsersList.tsx
+++ src/components/Users/UsersList.tsx
@@ -1,16 +1,21 @@
+import AddMemberButton from "./AddMemberButton"
 
 interface UserListProps {
   users: any[],
+  workspaceId: string
 }
 
-export default function UsersList({ users }: UserListProps) {
+export default function UsersList({ users, workspaceId }: UserListProps) {
   return (
     <ul>
       {
         users.length > 0 &&
         users.map((user) => {
           return (
-            <li key={user.id}>{user.name}</li>
+            <li className="flex justify-between" key={user.id}>
+              {user.name}
+              <AddMemberButton workspaceId={workspaceId} userId={user.id}/>
+            </li>
           )
         })
       
