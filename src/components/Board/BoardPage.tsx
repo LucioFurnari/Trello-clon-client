93ca0d7ade973d636e6a169bca79e5d4782f10c6
LucioFurnari <luciofurnari99@gmail.com>
2024-10-17 23:27:46 -0300

    feat: add getMembers function, and change props

--- src/components/Board/BoardPage.tsx
+++ src/components/Board/BoardPage.tsx
@@ -1,12 +1,14 @@
-import { getBoard } from "@/lib/board"
+import { getBoard } from "@/lib/board";
+import { getMembers } from "@/lib/workspace";
 import { notFound } from "next/navigation";
 import ListContainer from "../List/ListContainer";
 import ListContextProvider from "@/context/ListContext";
 import BoardHeader from "./BoarHeader";
 
 
-export default async function BoardPage({ params }: { params: { board: string }}) {
+export default async function BoardPage({ params }: { params: { board: string, workspace: string }}) {
   const data = await getBoard(params.board);
+  const members = (await getMembers(params.workspace) || []);
 
   if(!data) {
     notFound();
@@ -24,7 +26,7 @@ export default async function BoardPage({ params }: { params: { board: string }}
         : 'rgb(59 130 246 / var(--tw-bg-opacity))',
     }}
     className={" bg-blue-500 min-h-[calc(100vh-52px)] overflow-auto"}>
-      <BoardHeader title={data.title} />
+      <BoardHeader title={data.title} members={members}/>
       <ListContextProvider>
         <ListContainer lists={data.lists} />
       </ListContextProvider>
