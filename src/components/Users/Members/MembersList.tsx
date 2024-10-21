272097186eba5f476efbb97c7e982f1aca8e4d99
LucioFurnari <luciofurnari99@gmail.com>
2024-10-20 23:59:38 -0300

    feat: change z index of component

--- src/components/Users/Members/MembersList.tsx
+++ src/components/Users/Members/MembersList.tsx
@@ -6,7 +6,7 @@ interface MembersListProps {
   members: any[] | null,
 }
 
-export default function MembersList({ members }: MembersListProps ) {
+export default function MembersList({ members }: MembersListProps) {
   const [open, setOpen] = useState(false);
 
   function handleOpenMenu() {
@@ -14,20 +14,22 @@ export default function MembersList({ members }: MembersListProps ) {
   }
 
   return (
-    <div className="relative ml-10">
-      <button onClick={handleOpenMenu} className="bg-gray-300/50 hover:bg-gray-200/50 p-2 px-4 rounded">Members</button>
+    <div className="relative z-0 ml-10">
+      <button onClick={handleOpenMenu} className="bg-gray-300/50 hover:bg-gray-200/50 p-2 px-4 rounded">
+        Members
+      </button>
       {
         open &&
-        <ul className="absolute top-10 left-0 bg-gray-100 p-2 w-full">
-        {
-          members &&
-          members.map((member) => {
-            return (
-              <li key={member.id}>{member.name}</li>
-            )
-          })
-        }
-      </ul>
+        <ul className="absolute top-10 left-0 bg-gray-100 p-2 w-full z-50 shadow-lg">
+          {
+            members &&
+            members.map((member) => {
+              return (
+                <li key={member.id} className="p-2">{member.name}</li>
+              )
+            })
+          }
+        </ul>
       }
     </div>
   )
