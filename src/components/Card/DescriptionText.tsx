14fe7ca2a0bb5c0013df2507f925241b7815759a
LucioFurnari <luciofurnari99@gmail.com>
2024-10-21 17:38:51 -0300

    fix: fix description to show editor style

--- src/components/Card/DescriptionText.tsx
+++ src/components/Card/DescriptionText.tsx
@@ -1,15 +1,21 @@
-import Quill, { Delta } from "quill/core"
+import Quill, { Delta } from "quill/core";
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
-export default function DescriptionText({delta, setAction}: {delta: Delta | undefined, setAction: Dispatch<SetStateAction<boolean>>}) {
+export default function DescriptionText({
+  delta,
+  setAction,
+}: {
+  delta: Delta | undefined;
+  setAction: Dispatch<SetStateAction<boolean>>;
+}) {
   const quillRef = useRef<HTMLDivElement>(null);
-  const [html, setHtml] = useState('')
+  const [html, setHtml] = useState("");
 
   useEffect(() => {
     if (quillRef.current) {
       // Initialize Quill editor
       const quill = new Quill(quillRef.current, {
-        theme: 'snow',
+        theme: "snow",
         readOnly: true, // Set to true to make the editor read-only
         modules: {
           toolbar: false, // Disable the toolbar
@@ -19,22 +25,27 @@ export default function DescriptionText({delta, setAction}: {delta: Delta | unde
       if (delta) {
         // Set the editor contents from the delta object
         quill.setContents(delta);
+        // Extract the HTML content once Quill is ready
+        setHtml(quill.root.innerHTML);
       }
-      const htmlText = quill.getSemanticHTML();
-      setHtml(htmlText)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [delta]);
 
   return (
-  <div className="p-2 hover:bg-slate-400 cursor-pointer" onClick={() => setAction(true)}>
-    <div ref={quillRef} className="hidden"></div>
-    {
-      html === '<p></p>' ?
-      'Add a description'
-      :
-      <div className="ql-editor" dangerouslySetInnerHTML={{__html: html}}></div>
-    }
-  </div>
-  )
+    <div
+      className="p-2 hover:bg-slate-400 cursor-pointer"
+      onClick={() => setAction(true)}
+    >
+      <div ref={quillRef} className="hidden"></div>
+      {html === "" ? (
+        "Add a description"
+      ) : (
+        <div
+          className="ql-editor" // Make sure this class is applied for Quill's styling
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
+      )}
+    </div>
+  );
 }
