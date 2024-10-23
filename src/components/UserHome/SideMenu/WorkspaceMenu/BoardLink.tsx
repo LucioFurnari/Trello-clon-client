c99da00dd5e50ea6133dfea67d3e6cb679f9cedc
LucioFurnari <luciofurnari99@gmail.com>
2024-10-22 23:45:01 -0300

    feat: add workspace id prop

--- src/components/UserHome/SideMenu/WorkspaceMenu/BoardLink.tsx
+++ src/components/UserHome/SideMenu/WorkspaceMenu/BoardLink.tsx
@@ -2,14 +2,15 @@ import Link from "next/link"
 
 interface OptionMenuProps {
   boardId: string,
-  name: string
+  name: string,
+  workspaceId: string
 }
 
-export default function BoardLink({ boardId, name }: OptionMenuProps) {
+export default function BoardLink({ boardId, name, workspaceId }: OptionMenuProps) {
   return (
     <ul>
       <li>
-        <Link className="block text-left pl-6 py-1 text-zinc-300 hover:bg-slate-500/50" href={`/board/${boardId}`}>{name}</Link>
+        <Link className="block text-left pl-6 py-1 text-zinc-300 hover:bg-slate-500/50" href={`/workspace/${workspaceId}/board/${boardId}`}>{name}</Link>
       </li>
     </ul>
   )
