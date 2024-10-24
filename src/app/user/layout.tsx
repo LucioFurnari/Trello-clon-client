713253782c949cedadab9a04c8a2b9332384c6fb
LucioFurnari <luciofurnari99@gmail.com>
2024-10-23 22:57:25 -0300

    feat: add workspace children node

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -3,15 +3,18 @@ import WorkspaceContextProvider from "@/context/WorkspaceContext"
 export default function UserLayout({
   children,
   boards,
+  workspace,
 }: {
   children: React.ReactNode,
-  boards: React.ReactNode
+  boards: React.ReactNode,
+  workspace: React.ReactNode,
 }) {
   return (
     <main className="flex min-h-[calc(100vh-52px)] bg-slate-600">
       <WorkspaceContextProvider>
       {children}
       {boards}
+      {workspace}
       </WorkspaceContextProvider>
     </main>
   )
