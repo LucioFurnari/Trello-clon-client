0e9f9e3369308bb5526c1b19c5f3dff3e353c20a
LucioFurnari <luciofurnari99@gmail.com>
2024-10-28 21:08:01 -0300

    feat: remove tag

--- src/components/UserHome/UserHomePage.tsx
+++ src/components/UserHome/UserHomePage.tsx
@@ -2,8 +2,8 @@ import SideMenu from "./SideMenu/SideMenu"
 
 export default function UserHomePage() {
   return (
-    <div className="dark:bg-zinc-800">
+    <>
       <SideMenu />
-    </div>
+    </>
   )
 }
