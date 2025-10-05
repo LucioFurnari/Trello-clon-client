ffcf8039390271ac1eb11155b260c8d976befba3
LucioFurnari <luciofurnari99@gmail.com>
2025-10-05 18:39:09 -0300

    feat: remove props

--- src/components/BoardList/DeleteBoardButton.tsx
+++ src/components/BoardList/DeleteBoardButton.tsx
@@ -1,23 +1,13 @@
 
 import { deleteBoard } from "../../lib/board"
 
-interface DeleteBoardButtonProps {
+type DeleteBoardButtonProps = {
   id: string,
-  setAction: (value?: any) => void
 }
 
-export default function DeleteBoardButton({id, setAction}: DeleteBoardButtonProps) {
-
-  async function handleDeleteBoard() {
-    const res = await deleteBoard(id);
-
-    if (!res) {
-      return
-    }
-    setAction(id);
-  }
+export default function DeleteBoardButton({ id }: DeleteBoardButtonProps) {
 
   return (
-    <button onClick={handleDeleteBoard} className="bg-red-600 hover:bg-red-500 rounded p-2 px-8 text-lg mx-auto mt-4">Delete board</button>
+    <button onClick={() => deleteBoard(id)} className="bg-red-600 hover:bg-red-500 rounded p-2 px-8 text-lg mx-auto mt-4">Delete board</button>
   )
 }
