b817438ee8e6ba2bf98c18c605b778daffffb84e
LucioFurnari <luciofurnari99@gmail.com>
2025-10-05 18:08:08 -0300

    feat: remove useContext hook

--- src/components/UserHome/BoardsPage/WorkspaceItem.tsx
+++ src/components/UserHome/BoardsPage/WorkspaceItem.tsx
@@ -1,6 +1,5 @@
 import BoardList from "../../BoardList/BoardList"
 import { BoardData } from "@/types/types"
-import { useWorkspaceContext } from "@/context/WorkspaceContext"
 import { WorkspaceType } from "@/types/types"
 import WorkItemHeader from "@/components/Workspace/WorkItemHeader"
 
@@ -11,49 +10,11 @@ interface WorkspaceItemProp {
 }
 
 export function WorkspaceItem({ boards, workspaceId, workspace }: WorkspaceItemProp) {
-  const context = useWorkspaceContext();
-
-  function handleCreateBoard(board: any) {
-    context?.setWorkspace(context.workspace.map((workspace) => {
-      if (workspace.workspaceId === workspaceId) {
-        return {...workspace, boards: [...workspace.boards, board]}
-      } else {
-        return workspace;
-      }
-    }))
-  }
-
-  function handleDeleteBoard (boardId: string) {
-    context?.setWorkspace(context.workspace.map((workspace) => {
-      if (workspace.workspaceId === workspaceId) {
-        return {...workspace, boards: workspace.boards.filter(board => board.boardId !== boardId)}
-      } else {
-        return workspace;
-      }
-    }))
-  }
-
-  function handleDeleteWorkspace(id: string) {
-    context?.setWorkspace(context.workspace.filter((item) => item.workspaceId !== id));
-  }
-
-  function handleEditWorkspace(newData: any, id: string) {
-    context?.setWorkspace(context.workspace.map((item) => {
-      if (item.workspaceId === id) {
-        return {
-          ...item,
-          ...newData
-        }
-      } else {
-        return item
-      }
-    }))
-  }
 
   return (
     <div className="pb-4">
-      <WorkItemHeader workspaceId={workspaceId} workspace={workspace} handleDelete={handleDeleteWorkspace} handleEdit={handleEditWorkspace} />
-      <BoardList boards={boards} workspaceId={workspaceId} createHandle={handleCreateBoard} deleteHandle={handleDeleteBoard}/>
+      <WorkItemHeader workspaceId={workspaceId} workspace={workspace} />
+      <BoardList boards={boards} workspaceId={workspaceId}/>
     </div>
   )
 }
