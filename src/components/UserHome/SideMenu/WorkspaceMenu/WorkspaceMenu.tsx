4a73f7894c6a3f57acb81633ad03e22a51fcb613
LucioFurnari <luciofurnari99@gmail.com>
2025-10-05 18:10:18 -0300

    feat: add new props

--- src/components/UserHome/SideMenu/WorkspaceMenu/WorkspaceMenu.tsx
+++ src/components/UserHome/SideMenu/WorkspaceMenu/WorkspaceMenu.tsx
@@ -2,20 +2,23 @@
 
 import ListContainer from "./ListContainer"
 import CreateWorkspace from "./CreateWorkspace"
-import { useWorkspaceContext } from "@/context/WorkspaceContext"
+import { WorkspaceType, WorkspaceData } from "@/types/types"
 
-export default function WorkspaceMenu() {
-  const context = useWorkspaceContext();
+type WorkspaceMenuProps = {
+  workspaces: WorkspaceData[],
+};
+
+export default function WorkspaceMenu({ workspaces }: WorkspaceMenuProps) {
 
   return (
     <>
       <h3 className="text-zinc-400 text-xl border-b-[1px] border-gray-500/50 py-2 ml-2 mr-2">Workspaces</h3>
       {
-        context?.workspace &&
-        context.workspace.length > 0 &&
+        workspaces &&
+        workspaces.length > 0 &&
         <div>
           {
-            context.workspace.map((workspace) => {
+            workspaces.map((workspace: WorkspaceType) => {
               return(
                 <ListContainer key={workspace.workspaceId} name={workspace.name} id={workspace.workspaceId}/>
               )
